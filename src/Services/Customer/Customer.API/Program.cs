7db93a6f92850b4d16baf89b9a63f53b7165987d
Rolf Sjødal <rolf@sers.digital>
2021-05-12 12:22:45 +0200

    Add secrets read from k8s secret file

--- src/Services/Customer/Customer.API/Program.cs
+++ src/Services/Customer/Customer.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Customer.API
@@ -12,6 +13,10 @@ namespace Customer.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+             .ConfigureAppConfiguration((hostingContext, config) =>
+             {
+                 config.AddJsonFile("secrets/appsettings.secrets.json", optional: true);
+             })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
