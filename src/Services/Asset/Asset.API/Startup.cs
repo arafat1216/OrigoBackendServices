a81adc93276a4bece164f17e3bf1a422c33122ea
Rolf Sjødal <rolf@sers.digital>
2021-05-24 12:25:59 +0200

    Removed swagger document filter

--- src/Services/Asset/Asset.API/Startup.cs
+++ src/Services/Asset/Asset.API/Startup.cs
@@ -15,7 +15,7 @@ namespace Asset.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
@@ -59,7 +59,7 @@ namespace Asset.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
