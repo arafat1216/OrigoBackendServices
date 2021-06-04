bee4a3a0adba4cdeabc6c5cf45caa859e3682ee9
Rolf Sjødal <rolf@sers.digital>
2021-06-04 13:56:21 +0200

    Show displayname instead of firstname and lastname.

--- src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/OrigoUser.cs
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/OrigoUser.cs
@@ -8,8 +8,7 @@ namespace OrigoApiGateway.Models
         public OrigoUser(UserDTO user)
         {
             Id = user.Id;
-            FirstName = user.FirstName;
-            LastName = user.LastName;
+            DisplayName = user.FirstName + " " + user.LastName;
             Email = user.Email;
             MobileNumber = user.MobileNumber;
             EmployeeId = user.EmployeeId;
@@ -17,8 +16,7 @@ namespace OrigoApiGateway.Models
         }
 
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string DisplayName { get; set; }
         public string Email { get; set; }
         public string MobileNumber { get; set; }
         public string EmployeeId { get; set; }
