7ec9fbe811b3e92c5e1b1e8881abf5b59ac4c287
JuliaTeppen <julia@mytos.no>
2021-06-24 14:27:03 +0200

    Module object change

--- src/Services/Customer/CustomerServices/Models/ProductModuleGroup.cs
+++ src/Services/Customer/CustomerServices/Models/ProductModuleGroup.cs
@@ -1,9 +1,12 @@
 ﻿using Common.Seedwork;
+using System.Collections.Generic;
 
 namespace CustomerServices.Models
 {
     public class ProductModuleGroup : Entity
     {
         public string Name { get; set; }
+
+        public ICollection<Customer> Customers { get; set; }
     }
 }
