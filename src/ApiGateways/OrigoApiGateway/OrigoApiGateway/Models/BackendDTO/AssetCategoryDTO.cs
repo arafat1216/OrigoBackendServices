f7f4b32b2234a2fb6f61b82cdde33e185e888700
JuliaTeppen <julia@mytos.no>
2021-06-24 12:23:59 +0200

    Get asset categories

---
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/BackendDTO/AssetCategoryDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrigoApiGateway.Models.BackendDTO
+{
+    public class AssetCategoryDTO
+    {
+        /// <summary>
+        /// External id of the AssetCategory
+        /// </summary>
+        public Guid AssetCategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public bool UsesImei { get; set; }
+    }
+}
---
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/OrigoAssetCategory.cs
@@ -0,0 +1,26 @@
+﻿using OrigoApiGateway.Models.BackendDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrigoApiGateway.Models
+{
+    public class OrigoAssetCategory
+    {
+        public OrigoAssetCategory(AssetCategoryDTO assetCategory)
+        {
+            AssetCategoryId = assetCategory.AssetCategoryId;
+            Name = assetCategory.Name;
+            UsesImei = assetCategory.UsesImei;
+        }
+        /// <summary>
+        /// External id of the AssetCategory
+        /// </summary>
+        public Guid AssetCategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public bool UsesImei { get; set; }
+    }
+}
