68acf0ea676fca20ddab4c37c876dbc5ea54f943
JuliaTeppen <julia@mytos.no>
2021-07-28 15:09:01 +0200

    key vault helper class

---
+++ src/Core/AzureKeyVaultService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    class AzureKeyVaultService
+    {
+        private readonly IConfiguration Configuration;
+
+        public AzureKeyVaultService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+    }
+}
