309ce8354186bacedba68eedda0d7c03135ec10a
Mikael Antero Paavola Antero Paavola <mikael.paavola@mytos.no>
2021-09-12 20:02:24 +0200

    Added location file.

---
+++ src/Services/Customer/CustomerServices/Models/Location.cs
@@ -0,0 +1,44 @@
+﻿using Common.Seedwork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerServices.Models
+{
+    public class Location : Entity
+    {
+        // Todo: Set attributes and access modifiers
+        public Guid LocationId { get; protected set; }
+        public Guid CreatedBy { get; protected set; }
+        public Guid UpdatedBy { get; protected set; }
+
+        public string Name { get; protected set; }
+        public string? Description { get; protected set; }
+        public string? Address1 { get; protected set; }
+        public string? Address2 { get; protected set; }
+        public string? PostalCode { get; protected set; }
+        public string? City { get; protected set; }
+        public string? Country { get; protected set; }
+        public DateTime CreatedAt{ get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+
+        public Location(string name, string description, string address1, string address2, string postalCode, string city, string country)
+        {
+            LocationId = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            Address1 = address1;
+            Address2 = address2;
+            PostalCode = postalCode;
+            City = city;
+            Country = country;
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+            CreatedBy = Guid.NewGuid();  // todo: set these to user modifying the entity next us.
+            UpdatedBy = Guid.NewGuid();
+        }
+    }
+
+}
