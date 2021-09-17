483f09764b5a6b47edb9a933fbbe90b6688e46a8
JuliaTeppen <julia@mytos.no>
2021-09-17 09:17:19 +0200

    .

---
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Controllers/UserPermissionsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrigoApiGateway.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrigoApiGateway.Controllers
+{
+    public class UserPermissionsController : ControllerBase
+    {
+        private readonly ILogger<UserPermissionsController> _logger;
+        private readonly IUserPermissionServices _customerServices;
+
+        public UserPermissionsController(ILogger<UserPermissionsController> logger, IUserPermissionServices customerServices)
+        {
+            _logger = logger;
+            _customerServices = customerServices;
+        }
+    }
+}
--- src/Services/Customer/CustomerServices/IUserPermissionServices.cs
+++ src/Services/Customer/CustomerServices/IUserPermissionServices.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
+﻿using CustomerServices.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using CustomerServices.Models;
 
 namespace CustomerServices
 {
     public interface IUserPermissionServices
     {
         Task<IEnumerable<UserPermissions>> GetUserPermissionsAsync(string userName);
+        Task<UserPermissions> AssignUserPermissions(Guid userId, Role role, IList<Guid> accessList);
+        Task<UserPermissions> RemoveUserPermissions(Guid userId, Role role, IList<Guid> accessList);
     }
 }
