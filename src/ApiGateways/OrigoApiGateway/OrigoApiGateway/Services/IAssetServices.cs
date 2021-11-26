f12e9c6811872b4c8e51161684411967b1ec2fb4
JuliaTeppen <julia@mytos.no>
2021-11-26 09:36:47 +0100

    fix build issue

--- src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Services/IAssetServices.cs
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Services/IAssetServices.cs
@@ -8,7 +8,7 @@ namespace OrigoApiGateway.Services
 {
     public interface IAssetServices
     {
-        Task<IList<OrigoAsset>> GetAssetsForUserAsync(Guid customerId, Guid userId);
+        Task<IList<object>> GetAssetsForUserAsync(Guid customerId, Guid userId);
         Task<IList<object>> GetAssetsForCustomerAsync(Guid customerId);
         Task<OrigoPagedAssets> SearchForAssetsForCustomerAsync(Guid customerId, string search = "", int page = 1, int limit = 50);
         Task<OrigoAsset> GetAssetForCustomerAsync(Guid customerId, Guid assetId);
