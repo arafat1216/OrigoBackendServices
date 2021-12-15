23f1738d6d02af7ccde912fe6bc1ae6c5ea3cee1
Mikael Antero Paavola Antero Paavola <mikael.paavola@mytos.no>
2021-12-15 13:46:12 +0100

    Fixed decrepency between model in gateway and api

--- src/Services/Asset/Asset.API/ViewModels/UpdateAssetsStatus.cs
+++ src/Services/Asset/Asset.API/ViewModels/UpdateAssetsStatus.cs
@@ -13,8 +13,6 @@ namespace Asset.API.ViewModels
     {
         [Required]
         public IList<Guid> AssetGuidList { get; set; }
-        [Required]
-        public int AssetStatus { get; set; }
 
         /// <summary>
         /// id of user making the endpoint call. Can be ignored by frontend.
