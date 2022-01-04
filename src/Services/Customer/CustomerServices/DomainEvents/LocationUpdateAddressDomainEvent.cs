b9ba1e5aaa1a87c6264eb61a5833b3297ffbfa89
miakri <43073917+miakri@users.noreply.github.com>
2022-01-04 14:47:31 +0100

    Added domain events for Location and on creating a new department

---
+++ src/Services/Customer/CustomerServices/DomainEvents/LocationUpdateAddressDomainEvent.cs
@@ -0,0 +1,32 @@
+﻿using Common.Logging;
+using CustomerServices.Models;
+
+namespace CustomerServices.DomainEvents
+{
+    class LocationUpdateAddressDomainEvent : BaseEvent
+    {
+        public LocationUpdateAddressDomainEvent(Location location, int number, string oldAddress) : base(location.LocationId)
+        {
+            Location = location;
+            Number = number;
+            OldAddress = oldAddress;
+        }
+        public Location Location { get; protected set; }
+        public string OldAddress { get; protected set; }
+        public int Number { get; protected set; }
+        public override string EventMessage(string languageCode = "nb-NO")
+        {
+            if (Number == 1)
+            {
+                return $"Location address1 changed from {OldAddress} to {Location.Address1}.";
+            }
+            if (Number == 2)
+            { 
+                return $"Location address2 changed from {OldAddress} to {Location.Address2}.";
+            }
+            return string.Empty;
+
+
+        }
+    }
+}
---
+++ src/Services/Customer/CustomerServices/DomainEvents/LocationUpdateLocationNameDomainEvent.cs
@@ -0,0 +1,20 @@
+﻿using Common.Logging;
+using CustomerServices.Models;
+
+namespace CustomerServices.DomainEvents
+{
+    class LocationUpdateNameDomainEvent : BaseEvent
+    {
+        public LocationUpdateNameDomainEvent(Location location, string oldName) : base(location.LocationId)
+        {
+            Location = location;
+            OldName = oldName;
+        }
+        public Location Location { get; protected set; }
+        public string OldName { get; protected set; }
+        public override string EventMessage(string languageCode = "nb-NO")
+        {
+            return $"Location name changed from {OldName} to {Location.Name}.";
+        }
+    }
+}
