7c3e5a61d0f0bc8ba2bdd6ad182a47c4533e83cd
Dblaze47 <manosh.office015@gmail.com>
2022-01-27 16:04:17 +0600

    Adds Entry product and maps asset and user management to it

--- src/Services/ProductCatalog/ProductCatalog.Infrastructure/Migrations/20220127095011_add_entry_product_seeddata.Designer.cs
+++ src/Services/ProductCatalog/ProductCatalog.Infrastructure/Migrations/20220127095011_add_entry_product_seeddata.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductCatalog.Infrastructure.Infrastructure.Context;
 
@@ -11,9 +12,10 @@ using ProductCatalog.Infrastructure.Infrastructure.Context;
 namespace ProductCatalog.Infrastructure.Migrations
 {
     [DbContext(typeof(ProductCatalogContext))]
-    partial class ProductCatalogContextModelSnapshot : ModelSnapshot
+    [Migration("20220127095011_add_entry_product_seeddata")]
+    partial class add_entry_product_seeddata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
