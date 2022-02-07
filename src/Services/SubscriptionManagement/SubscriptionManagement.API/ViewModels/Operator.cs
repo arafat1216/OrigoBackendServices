eec5e4aac2e5bf532cf4652be3e934e985030c0f
Atish Kumar Dipongkor <atish@brainstation-23.com>
2022-02-07 14:47:31 +0600

    get operator response changed to object instead of string

--- src/Services/SubscriptionManagement/SubscriptionManagement.API/ViewModels/Operator.cs
+++ src/Services/SubscriptionManagement/SubscriptionManagement.API/ViewModels/Operator.cs
@@ -1,10 +1,12 @@
-﻿using SubscriptionManagementServices.Models;
-
-namespace SubscriptionManagement.API.ViewModels
+﻿namespace SubscriptionManagement.API.ViewModels
 {
     public record Operator
     {
-
+        public Operator(SubscriptionManagementServices.Models.Operator @operator)
+        {
+            OperatorName = @operator.OperatorName;
+            Country = @operator.Country;
+        }
         public string OperatorName { get; set; }
         public string Country { get; set; }
 
