2ec2eb817fddf2cd6f54346fcf2c0e8c011a132c
miakri <43073917+miakri@users.noreply.github.com>
2022-02-20 20:49:04 +0100

    Added get for all operators subscription products

---
+++ src/Services/SubscriptionManagement/SubscriptionManagementServices/Mappings/GlobalSubscriptionProductProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SubscriptionManagementServices.Models;
+using SubscriptionManagementServices.ServiceModels;
+
+namespace SubscriptionManagementServices.Mappings
+{
+    public class GlobalSubscriptionProductProfile : Profile
+    {
+        public GlobalSubscriptionProductProfile()
+        {
+            CreateMap<SubscriptionProduct, GlobalSubscriptionProductDTO>()
+           .ForMember(destination => destination.Datapackages,
+               opt => opt.MapFrom(src => src.DataPackages.Select(s => s.DataPackageName)))
+           .ForMember(destination => destination.Name,
+               opt => opt.MapFrom(src => src.SubscriptionName))
+           .ForMember(destination => destination.OperatorId,
+               opt => opt.MapFrom(src => src.Operator.Id));
+        }
+    }
+}
