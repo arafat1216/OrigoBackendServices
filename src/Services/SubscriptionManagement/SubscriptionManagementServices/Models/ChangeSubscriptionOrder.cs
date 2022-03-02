e8fda8947baa2a6600acece5a320cb501e303b3c
miakri <43073917+miakri@users.noreply.github.com>
2022-03-02 13:38:40 +0100

    Added change subscription order model

---
+++ src/Services/SubscriptionManagement/SubscriptionManagementServices/Models/ChangeSubscriptionOrder.cs
@@ -0,0 +1,44 @@
+﻿using Common.Seedwork;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SubscriptionManagementServices.Models
+{
+    public class ChangeSubscriptionOrder : Entity, ISubscriptionOrder
+    {
+
+        public ChangeSubscriptionOrder()
+        {
+        }
+
+        public ChangeSubscriptionOrder(string mobileNumber, string productName, string packageName, string operatorName, DateTime orderExecutionDate, string orderAuthor, Guid organizationId)
+        {
+            MobileNumber = mobileNumber;
+            ProductName = productName;
+            PackageName = packageName;
+            OperatorName = operatorName;
+            OrderExecutionDate = orderExecutionDate;
+            OrderAuthor = orderAuthor;
+            OrganizationId = organizationId;
+        }
+
+        public string MobileNumber { get; set; }
+        public string ProductName { get; set; }
+        public string PackageName { get; set; }
+        public string OperatorName { get; set; }
+        public DateTime OrderExecutionDate { get; set; }
+        public string OrderAuthor { get; set; }
+        public Guid OrganizationId { get; set; }
+
+
+        #region ISubscriptionOrder Implementation
+
+        [NotMapped] public string OrderType => "ChangeSubscription";
+
+        [NotMapped] public string PhoneNumber => MobileNumber;
+
+        [NotMapped] public string NewSubscriptionOrderOwnerName => OrderAuthor;
+
+        [NotMapped] public DateTime TransferDate => OrderExecutionDate;
+        #endregion
+    }
+}
