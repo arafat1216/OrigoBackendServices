1ef8f8fd432b0840996618660fad37a2065a9f1f
miakri <43073917+miakri@users.noreply.github.com>
2022-03-03 13:14:05 +0100

    added migration for change subscription order

--- src/Services/SubscriptionManagement/SubscriptionManagementServices/Migrations/20220303121108_Add_ChangeSubscriptionOrder_Table.Designer.cs
+++ src/Services/SubscriptionManagement/SubscriptionManagementServices/Migrations/20220303121108_Add_ChangeSubscriptionOrder_Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SubscriptionManagementServices.Infrastructure;
 
@@ -11,9 +12,10 @@ using SubscriptionManagementServices.Infrastructure;
 namespace SubscriptionManagementServices.Migrations
 {
     [DbContext(typeof(SubscriptionManagementContext))]
-    partial class SubscriptionManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20220303121108_Add_ChangeSubscriptionOrder_Table")]
+    partial class Add_ChangeSubscriptionOrder_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
