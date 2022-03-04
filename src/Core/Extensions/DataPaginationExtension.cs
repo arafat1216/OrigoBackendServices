32deb8683be232ff14682cc7bb8019c77cfb9934
Rolf Sjødal <rolf@sers.digital>
2022-03-04 14:53:38 +0100

    Comment cleanup

--- src/Core/Extensions/DataPaginationExtension.cs
+++ src/Core/Extensions/DataPaginationExtension.cs
@@ -5,38 +5,37 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
+namespace Common.Extensions;
+
 /// <summary>
 /// Extension method for returning paginated results. From https://vmsdurano.com/asp-net-core-5-implement-web-api-pagination-with-hateoas-links/
 /// </summary>
-namespace Common.Extensions
+public static class DataPaginationExtension
 {
-    public static class DataPaginationExtension
+    public static async Task<PagedModel<TModel>> PaginateAsync<TModel>(
+        this IQueryable<TModel> query,
+        int page,
+        int limit,
+        CancellationToken cancellationToken)
+        where TModel : class
     {
-        public static async Task<PagedModel<TModel>> PaginateAsync<TModel>(
-            this IQueryable<TModel> query,
-            int page,
-            int limit,
-            CancellationToken cancellationToken)
-            where TModel : class
-        {
 
-            var paged = new PagedModel<TModel>();
+        var paged = new PagedModel<TModel>();
 
-            page = (page < 0) ? 1 : page;
+        page = (page < 0) ? 1 : page;
 
-            paged.CurrentPage = page;
-            paged.PageSize = limit;
+        paged.CurrentPage = page;
+        paged.PageSize = limit;
 
-            var startRow = (page - 1) * limit;
-            paged.Items = await query
-                       .Skip(startRow)
-                       .Take(limit)
-                       .ToListAsync(cancellationToken);
+        var startRow = (page - 1) * limit;
+        paged.Items = await query
+            .Skip(startRow)
+            .Take(limit)
+            .ToListAsync(cancellationToken);
 
-            paged.TotalItems = await query.CountAsync(cancellationToken);
-            paged.TotalPages = (int) Math.Ceiling(paged.TotalItems / (double)limit);
+        paged.TotalItems = await query.CountAsync(cancellationToken);
+        paged.TotalPages = (int) Math.Ceiling(paged.TotalItems / (double)limit);
 
-            return paged;
-        }
+        return paged;
     }
 }
