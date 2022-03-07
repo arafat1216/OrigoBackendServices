5e9a95737a9102def87c3d9dd8ec88941d32614b
miakri <43073917+miakri@users.noreply.github.com>
2022-03-07 15:45:55 +0100

    added for dummy endpoint for frontend

---
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/SubscriptionManagement/Backend/Request/ActivateSimOrderPostRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace OrigoApiGateway.Models.SubscriptionManagement.Backend.Request
+{
+    public record ActivateSimOrderPostRequest
+    {
+        public string MobileNumber { get; set;}
+        public int OperatorId { get; set; }
+        public string SimNumber { get; set; }
+        public string SimType { get; set; }
+        
+        
+  }
+}
