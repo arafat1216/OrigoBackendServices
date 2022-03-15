0e9abb84feb97ed31a70ac533bdf90503843e46c
miakri <43073917+miakri@users.noreply.github.com>
2022-03-15 17:09:32 +0100

    added post newSubscriptionOrder in gateway

---
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/SubscriptionManagement/Backend/Request/NewSubscriptionOrderPostRequest.cs
@@ -0,0 +1,38 @@
+﻿using OrigoApiGateway.Models.SubscriptionManagement.Frontend.Request;
+using System;
+using System.Collections.Generic;
+
+namespace OrigoApiGateway.Models.SubscriptionManagement.Backend.Request
+{
+    public record NewSubscriptionOrderPostRequest
+    {
+        public string MobileNumber { get; set; }
+        
+        public int OperatorId { get; set; }
+       
+        public int? OperatorAccountId { get; set; }
+       
+        public NewOperatorAccountRequested? NewOperatorAccount { get; set; }
+
+        public int SubscriptionProductId { get; set; }
+
+      
+        public string? DataPackage { get; set; }
+        
+        public DateTime OrderExecutionDate { get; set; }
+
+        public IList<string> AddOnProducts { get; set; } = new List<string>();
+
+        public string? SimCardNumber { get; set; }
+
+        
+        public string SimCardAction { get; set; }
+        
+        public SimCardAddress? SimCardAddress { get; set; } = null;
+
+        public IList<NewCustomerReferenceValue> CustomerReferenceFields { get; set; } = new List<NewCustomerReferenceValue>();
+
+        public PrivateSubscription? PrivateSubscription { get; set; } = null;
+        public Guid CallerId { get; set; }
+    }
+}
---
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/SubscriptionManagement/Frontend/Request/SimCardAddress.cs
@@ -0,0 +1,12 @@
+﻿namespace OrigoApiGateway.Models.SubscriptionManagement.Frontend.Request
+{
+    public class SimCardAddress
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? PostalCode { get; set; }
+        public string? PostalPlace { get; set; }
+        public string? Country { get; set; }
+    }
+}
