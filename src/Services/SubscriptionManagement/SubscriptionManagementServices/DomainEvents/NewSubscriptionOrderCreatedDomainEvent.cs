90499d08565882c5730f1b7c69a2b150794f7da8
miakri <43073917+miakri@users.noreply.github.com>
2022-03-15 15:11:04 +0100

    changed domain event EventMessage

--- src/Services/SubscriptionManagement/SubscriptionManagementServices/DomainEvents/NewSubscriptionOrderCreatedDomainEvent.cs
+++ src/Services/SubscriptionManagement/SubscriptionManagementServices/DomainEvents/NewSubscriptionOrderCreatedDomainEvent.cs
@@ -15,7 +15,7 @@ namespace SubscriptionManagementServices.DomainEvents
         public NewSubscriptionOrder NewSubscriptionOrder { get; set; }
         public Guid CallerId { get; set; }
 
-        public override string EventMessage(string languageCode = "nb-NO")
+        public override string EventMessage()
         {
             return $"Customer with id {NewSubscriptionOrder.OrganizationId} added a {NewSubscriptionOrder.OrderType} order for mobile number {NewSubscriptionOrder.MobileNumber}";
         }
