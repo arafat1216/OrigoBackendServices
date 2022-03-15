90c232eed6705ac722a2d524834efb80d4093191
Atish Kumar Dipongkor <atish@brainstation-23.com>
2022-03-15 18:57:06 +0600

    merge with develop

--- src/Services/SubscriptionManagement/SubscriptionManagementServices/DomainEvents/ActivateSimOrderCreatedDomainEvent.cs
+++ src/Services/SubscriptionManagement/SubscriptionManagementServices/DomainEvents/ActivateSimOrderCreatedDomainEvent.cs
@@ -14,7 +14,7 @@ namespace SubscriptionManagementServices.DomainEvents
         public ActivateSimOrder ActivateSimOrder { get; set; }
         public Guid CallerId { get; set; }
 
-        public override string EventMessage(string languageCode = "nb-NO")
+        public override string EventMessage()
         {
             return $"Customer with id {ActivateSimOrder.OrganizationId} added a new {ActivateSimOrder.OrderType} for {ActivateSimOrder.MobileNumber}";
         }
