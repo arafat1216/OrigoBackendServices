29bc04305a2a204a1bdfa8b5e95f72e2b0728240
Atish Kumar Dipongkor <atish@brainstation-23.com>
2022-03-18 21:29:30 +0600

    OrigoSubscriptionProduct model reversed

--- src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/SubscriptionManagement/OrigoSubscriptionProduct.cs
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/SubscriptionManagement/OrigoSubscriptionProduct.cs
@@ -5,9 +5,9 @@ namespace OrigoApiGateway.Models.SubscriptionManagement
     public record OrigoSubscriptionProduct
     {
         public int Id { get; set; }
-        public string SubscriptionName { get; set; }
+        public string Name { get; set; }
         public int OperatorId { get; set; }
-        public IList<string>? Datapackages { get; set; }
+        public IList<string>? DataPackages { get; set; }
         public bool IsGlobal { get; set; }
     }
 }
