5974c9a04643cc6b293fb669c44bf4333644f447
Christian <christian@mytos.no>
2022-03-21 13:12:54 +0100

    Added missing country validation attribute to input models

--- src/Services/Customer/Customer.API/ViewModels/NewOrganizationPreferences.cs
+++ src/Services/Customer/Customer.API/ViewModels/NewOrganizationPreferences.cs
@@ -1,4 +1,6 @@
-﻿namespace Customer.API.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Customer.API.ViewModels
 {
     public class NewOrganizationPreferences
     {
@@ -6,7 +8,10 @@
         public string LogoUrl { get; set; }
         public string OrganizationNotes { get; set; }
         public bool? EnforceTwoFactorAuth { get; set; }
+
+        [RegularExpression("^[a-z]{2}")] // Exactly 2 lowercase characters
         public string PrimaryLanguage { get; set; }
+
         public short? DefaultDepartmentClassification { get; set; }
     }
 }
