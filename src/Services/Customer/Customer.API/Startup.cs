10f95d2a7491b0cb681663c744bade8dd540dfbd
Christian <christian@mytos.no>
2022-03-22 10:05:37 +0100

    Registered lowercase URL preference for better consistency

--- src/Services/Customer/Customer.API/Startup.cs
+++ src/Services/Customer/Customer.API/Startup.cs
@@ -33,6 +33,7 @@ namespace Customer.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting(options => options.LowercaseUrls = true);
 
             services.AddControllers().AddDapr();
             services.AddApiVersioning(config =>
