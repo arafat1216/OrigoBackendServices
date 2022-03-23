a74f63b3eaf56ac2c6bd0f4cad87462215d10caa
Christian <christian@mytos.no>
2022-03-23 11:16:12 +0100

    Added updated migration files

--- src/Services/Customer/CustomerServices/Migrations/20220323101240_updated_entity_configuration.Designer.cs
+++ src/Services/Customer/CustomerServices/Migrations/20220323101240_updated_entity_configuration.Designer.cs
@@ -4,6 +4,7 @@ using CustomerServices.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CustomerServices.Migrations
 {
     [DbContext(typeof(CustomerContext))]
-    partial class CustomerContextModelSnapshot : ModelSnapshot
+    [Migration("20220323101240_updated_entity_configuration")]
+    partial class updated_entity_configuration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
