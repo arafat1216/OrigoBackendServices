1d2bfdce9038bf00b53731d6af84be176ad07345
Atish Kumar Dipongkor <atish@brainstation-23.com>
2022-04-13 12:42:54 +0600

    setting endpoint added

---
+++ HardwareRepair.API/Controllers/HardwareRepairController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace HardwareRepair.API.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class HardwareRepairController : ControllerBase
+    {
+        [Route("{customerId:Guid}/settings")]
+        [HttpPost]
+        [SwaggerOperation(Tags = new[] { "Settings" })]
+        public async Task<IActionResult> ConfigureSettings(Guid customerId)
+        {
+            return Ok();
+        }
+
+        [Route("{customerId:Guid}/settings")]
+        [HttpGet]
+        [SwaggerOperation(Tags = new[] { "Settings" })]
+        public async Task<IActionResult> GetSettings(Guid customerId)
+        {
+            return Ok();
+        }
+    }
+}
--- HardwareRepair.API/Program.cs
+++ HardwareRepair.API/Program.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 
 var apiVersion = new ApiVersion(1, 0);
 var builder = WebApplication.CreateBuilder(args);
@@ -10,3 +11,63 @@ builder.Configuration.AddUserSecrets<Program>(optional: true);
 
 builder.Services.AddHealthChecks();
 builder.Services.AddControllers().AddDapr();
+
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddApiVersioning(config =>
+{
+    config.DefaultApiVersion = apiVersion;
+    config.AssumeDefaultVersionWhenUnspecified = true;
+});
+
+builder.Services.AddMvc();
+
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc($"v{apiVersion.MajorVersion}", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Hardware Repair",
+        Version = $"v{apiVersion.MajorVersion}"
+    });
+
+    var xmlComments = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    config.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlComments));
+    config.EnableAnnotations();
+});
+
+
+builder.Services.AddApplicationInsightsTelemetry();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Program>(optional: true);
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint($"/swagger/v{apiVersion.MajorVersion}/swagger.json",
+                $"Hardware Repair Services v{apiVersion.MajorVersion}"));
+
+}
+app.UseHealthChecks("/healthz");
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHealthChecks("/healthz");
+    endpoints.MapControllers();
+});
+
+app.Run();
