a5b9324747dfb223dd934100e81a2813341b90c8
Atish Kumar Dipongkor <atish@brainstation-23.com>
2022-04-25 17:33:22 +0600

    Unit tests added

---
+++ src/Services/HardwareServiceOrder/Tests/HardwareServiceOrder.UnitTests/HardwareServiceOrderServiceBaseTests.cs
@@ -0,0 +1,25 @@
+﻿using HardwareServiceOrderServices.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HardwareServiceOrder.UnitTests
+{
+    public class HardwareServiceOrderServiceBaseTests
+    {
+        protected readonly Guid CUSTOMER_ONE_ID = new("42447F76-D9A8-4F0A-B0FF-B4683ACEDD62");
+        protected readonly Guid CALLER_ONE_ID = new("42447F76-D9A8-4F0A-B0FF-B4683ACEDD63");
+        protected HardwareServiceOrderServiceBaseTests(DbContextOptions<HardwareServiceOrderContext> dbContext)
+        {
+            ContextOptions = dbContext;
+            Seed();
+        }
+        protected DbContextOptions<HardwareServiceOrderContext> ContextOptions { get; }
+
+        private void Seed()
+        {
+            using var context = new HardwareServiceOrderContext(ContextOptions);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+}
