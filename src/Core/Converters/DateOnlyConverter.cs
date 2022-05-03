87c7081e41feac4617e34c1b02aa718d0f0e4c96
Christian <christian@mytos.no>
2022-05-03 12:15:19 +0200

    Added JSON serializer converters for DateOnly and TimeOnly

---
+++ src/Core/Converters/DateOnlyConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Common.Converters
+{
+    // Source: https://marcominerva.wordpress.com/2021/11/22/dateonly-and-timeonly-support-with-system-text-json/
+
+    public class DateOnlyConverter : JsonConverter<DateOnly>
+    {
+        private readonly string serializationFormat;
+
+        public DateOnlyConverter() : this(null)
+        {
+        }
+
+        public DateOnlyConverter(string? serializationFormat)
+        {
+            this.serializationFormat = serializationFormat ?? "yyyy-MM-dd";
+        }
+
+        public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            return DateOnly.Parse(value!);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+            => writer.WriteStringValue(value.ToString(serializationFormat));
+    }
+}
---
+++ src/Core/Converters/TimeOnlyConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Common.Converters
+{
+    // Source: https://marcominerva.wordpress.com/2021/11/22/dateonly-and-timeonly-support-with-system-text-json/
+
+    public class TimeOnlyConverter : JsonConverter<TimeOnly>
+    {
+        private readonly string serializationFormat;
+
+        public TimeOnlyConverter() : this(null)
+        {
+        }
+
+        public TimeOnlyConverter(string? serializationFormat)
+        {
+            this.serializationFormat = serializationFormat ?? "HH:mm:ss.fff";
+        }
+
+        public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            return TimeOnly.Parse(value!);
+        }
+
+        public override void Write(Utf8JsonWriter writer, TimeOnly value, JsonSerializerOptions options)
+            => writer.WriteStringValue(value.ToString(serializationFormat));
+    }
+}
