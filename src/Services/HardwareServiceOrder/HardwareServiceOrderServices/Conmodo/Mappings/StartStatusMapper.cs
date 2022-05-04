c8daea2d00e450d849d1abd280897a5a06a9a3b3
Christian <christian@mytos.no>
2022-05-04 11:21:10 +0200

    Added Conmodo mapping classes

---
+++ src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Conmodo/Mappings/StartStatusMapper.cs
@@ -0,0 +1,21 @@
+﻿using HardwareServiceOrderServices.Conmodo.ApiModels;
+using HardwareServiceOrderServices.Models;
+
+namespace HardwareServiceOrderServices.Conmodo.Mappings
+{
+    internal class StartStatusMapper
+    {
+        public StartStatus FromServiceType(int serviceTypeId)
+        {
+            switch (serviceTypeId)
+            {
+                case (int)ServiceTypeEnum.SUR:
+                    return new StartStatus(25007, "Garanti?");
+                case (int)ServiceTypeEnum.SWAP:
+                    return new StartStatus(25456, "Preswap?");
+                default:
+                    throw new NotSupportedException("This provider don't support this service-type.");
+            }
+        }
+    }
+}
