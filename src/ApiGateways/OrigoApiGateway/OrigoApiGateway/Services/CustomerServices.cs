c7d8954d00d3f644da0318411d1081b5bfc0ba4b
miakri <43073917+miakri@users.noreply.github.com>
2022-05-13 10:03:17 +0200

    Added NOK as default if user dont have address or location

--- src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Services/CustomerServices.cs
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Services/CustomerServices.cs
@@ -345,12 +345,14 @@ namespace OrigoApiGateway.Services
         public async Task<string> GetCurrencyByCustomer(Guid customerId)
         {
             var customer = await GetCustomerAsync(customerId);
-            if (customer.Location == null) return CurrencyCode.EUR.ToString();
+            if (customer.Location == null) return CurrencyCode.NOK.ToString();
             
             var country = customer.Location.Country;
             if (string.IsNullOrEmpty(country))
-                country = customer.Address.Country;
 
+            if (customer.Address == null) return CurrencyCode.NOK.ToString();
+
+            country = customer.Address.Country;
             if (string.IsNullOrEmpty(country))
                 return CurrencyCode.NOK.ToString();
 
