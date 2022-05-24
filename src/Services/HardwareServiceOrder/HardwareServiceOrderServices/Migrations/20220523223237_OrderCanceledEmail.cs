3d69b1093dd95804778b45a87e3a8a957e343cb8
Atish Kumar Dipongkor <atish@brainstation-23.com>
2022-05-24 14:59:12 +0600

    migration added

---
+++ src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Migrations/20220523223237_OrderCanceledEmail.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HardwareServiceOrderServices.Migrations
+{
+    public partial class OrderCanceledEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssetName",
+                table: "HardwareServiceOrders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOrderCancellationEmailSent",
+                table: "HardwareServiceOrders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssetName",
+                table: "HardwareServiceOrders");
+
+            migrationBuilder.DropColumn(
+                name: "IsOrderCancellationEmailSent",
+                table: "HardwareServiceOrders");
+        }
+    }
+}
