b50f9dfa792bf0f4aeb377800dcc36d22fdb444c
miakri <43073917+miakri@users.noreply.github.com>
2022-05-30 12:02:19 +0200

    reversed on down and rebased onto trunk

---
+++ src/Services/Customer/CustomerServices/Migrations/20220530065418_updateRoleNames.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerServices.Migrations
+{
+    public partial class updateRoleNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql("UPDATE UserPermissions SET RoleId = 8 Where RoleId = 2");
+            migrationBuilder.Sql("UPDATE UserPermissions SET RoleId = 9 Where RoleId = 3");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE UserPermissions SET RoleId = 2 Where RoleId = 8");
+            migrationBuilder.Sql("UPDATE UserPermissions SET RoleId = 3 Where RoleId = 9");
+        }
+    }
+}
