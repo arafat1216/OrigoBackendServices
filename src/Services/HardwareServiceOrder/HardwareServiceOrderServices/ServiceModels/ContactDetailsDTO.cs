7ac45838a07f0546bcd8fd9c0e5206c933df75cb
Atish Kumar Dipongkor <atish@brainstation-23.com>
2022-06-09 17:33:32 +0600

    minor changes

---
+++ src/Services/HardwareServiceOrder/HardwareServiceOrderServices/ServiceModels/ContactDetailsDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareServiceOrderServices.ServiceModels
+{
+    public class ContactDetailsDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public Guid Id { get; set; }
+        public string? PhoneNumber { get; set; }
+        public Guid OrganizationId { get; set; }
+        public string OrganizationName { get; set; }
+        public string? OrganizationNumber { get; set; }
+        public Guid PartnerId { get; set; }
+        public string PartnerName { get; set; }
+        public string PartnerOrganizationNumber { get; set; }
+    }
+}
