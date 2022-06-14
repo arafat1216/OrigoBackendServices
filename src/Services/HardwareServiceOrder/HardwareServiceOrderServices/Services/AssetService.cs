b39a2b89d8dc83142097166c239f93eec7283f59
Christian <christian@mytos.no>
2022-06-14 11:39:13 +0200

    Added the new methods for calling the asset service

--- src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Services/AssetService.cs
+++ src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Services/AssetService.cs
@@ -1,7 +1,9 @@
 ﻿using Common.Enums;
 using HardwareServiceOrderServices.Configuration;
+using HardwareServiceOrderServices.Models;
 using Microsoft.Extensions.Options;
 using System.Net.Http.Json;
+using Common.Extensions;
 
 namespace HardwareServiceOrderServices.Services
 {
@@ -25,6 +27,7 @@ namespace HardwareServiceOrderServices.Services
         }
 
         /// <inheritdoc cref="UpdateAssetLifeCycleStatusAsync(Guid,Guid, AssetLifecycleStatus)"/>
+        [Obsolete]
         public async Task UpdateAssetLifeCycleStatusAsync(Guid customerId, Guid assetLifeCycleId, AssetLifecycleStatus status)
         {
             var requestBody = new List<Guid> { assetLifeCycleId };
@@ -40,6 +43,64 @@ namespace HardwareServiceOrderServices.Services
 
             //    throw;
             //}
+        }
+
+        /// <inheritdoc/>
+        public async Task UpdateAssetLifeCycleStatusAsync(Guid assetLifecycleId, ServiceStatusEnum newServiceStatus, IEnumerable<string>? newImeis, string? newSerialNumber)
+        {
+            HttpResponseMessage? result = null;
+
+            switch (newServiceStatus)
+            {
+                // No assigned value: Throw an error!
+                case ServiceStatusEnum.Null:
+                    throw new ArgumentNullException(nameof(newServiceStatus), "The service status was not provided.");
+
+                // New service-registration
+                case ServiceStatusEnum.Registered:
+                    JsonContent content = JsonContent.Create(Guid.Empty.SystemUserId());
+                    result = await _httpClient.PatchAsync($"{_config.ApiPath}/{assetLifecycleId}/send-to-repair", content);
+                    break;
+
+                // These don't trigger any changes inside the asset-microservice
+                case ServiceStatusEnum.Unknown:
+                case ServiceStatusEnum.RegisteredInTransit:
+                case ServiceStatusEnum.RegisteredUserActionNeeded:
+                case ServiceStatusEnum.Ongoing:
+                case ServiceStatusEnum.OngoingUserActionNeeded:
+                case ServiceStatusEnum.OngoingInTransit:
+                case ServiceStatusEnum.OngoingReadyForPickup:
+                    break;
+
+                // Repair completed
+                case ServiceStatusEnum.Canceled:
+                case ServiceStatusEnum.CompletedNotRepaired:
+                case ServiceStatusEnum.CompletedRepaired:
+                case ServiceStatusEnum.CompletedRepairedOnWarranty:
+                    result = await _httpClient.PutAsJsonAsync($"{_config.ApiPath}/{assetLifecycleId}/repair-completed", new { CallerId = Guid.Empty.SystemUserId(), Discarded = false });
+                    break;
+
+                // Device replaced
+                case ServiceStatusEnum.CompletedReplaced:
+                case ServiceStatusEnum.CompletedReplacedOnWarranty:
+                    result = await _httpClient.PutAsJsonAsync($"{_config.ApiPath}/{assetLifecycleId}/repair-completed", new { CallerId = Guid.Empty.SystemUserId(), Discarded = false, NewImei = newImeis, NewSerialNumber = newSerialNumber });
+                    break;
+
+                // Device discarded
+                case ServiceStatusEnum.CompletedDiscarded:
+                    result = await _httpClient.PutAsJsonAsync($"{_config.ApiPath}/{assetLifecycleId}/repair-completed", new { CallerId = Guid.Empty.SystemUserId(), Discarded = true });
+                    break;
+
+                // Unsupported & default actions
+                case ServiceStatusEnum.CompletedCredited:
+                default:
+                    throw new NotImplementedException("This is currently not supported.");
+            }
+
+            if (result is not null && !result.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException("The request did not return a success code!");
+            }
         }
     }
 }
--- src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Services/IAssetService.cs
+++ src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Services/IAssetService.cs
@@ -1,4 +1,5 @@
 ﻿using Common.Enums;
+using HardwareServiceOrderServices.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,21 @@ namespace HardwareServiceOrderServices.Services
         /// <param name="assetLifeCycleId">Asset life-cycle identifier</param>
         /// <param name="status">New status of the asset life-cycle</param>
         /// <returns></returns>
+        [Obsolete]
         Task UpdateAssetLifeCycleStatusAsync(Guid customerId, Guid assetLifeCycleId, AssetLifecycleStatus status);
+
+
+        /// <summary>
+        ///     Connects to the Asset-microservice, notifying it about new and updated service-statuses.
+        /// </summary>
+        /// <param name="assetLifecycleId"> The ID for the asset-lifecycle. </param>
+        /// <param name="newServiceStatus"> The new service-status. </param>
+        /// <param name="newImeis"> If the device has been replaced, a list of the new IMEI numbers. </param>
+        /// <param name="newSerialNumber"> If the device has been replaced, the new serial-number. </param>
+        /// <returns> The awaitable task. </returns>
+        /// <exception cref="ArgumentNullException"> Thrown when <paramref name="newServiceStatus"/> has a <see langword="null"/> equivalent. </exception>
+        /// <exception cref="NotImplementedException"> Thrown when a not-supported, or not implemented (out of scope) service-status is triggered. </exception>
+        /// <exception cref="HttpRequestException"> Thrown when we fail to update the asset microservice. </exception>
+        Task UpdateAssetLifeCycleStatusAsync(Guid assetLifecycleId, ServiceStatusEnum newServiceStatus, IEnumerable<string>? newImeis, string? newSerialNumber);
     }
 }
