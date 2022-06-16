097051d0f96afe7f651492af413e07ee19c7e570
Atish Kumar Dipongkor <atish@brainstation-23.com>
2022-06-16 09:12:41 +0600

    sur request dto removed

--- src/Services/HardwareServiceOrder/HardwareServiceOrder.API/ViewModels/SURRequestDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HardwareServiceOrder.API.ViewModels
-{
-    public class SURRequestDTO
-    {
-        public CustomerSettings CustomerSettings { get; set; }
-        public CustomerServiceProvider CustomerServiceProvider { get; set; }
-    }
-}
