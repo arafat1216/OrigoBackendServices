ff26694113404ce0e4a2043bb3cd040e75296a30
Atish Kumar Dipongkor <atish@brainstation-23.com>
2022-06-21 16:15:07 +0600

    minor changes

--- src/Services/HardwareServiceOrder/Tests/HardwareServiceOrder.IntegrationTests/HardwareServiceOrderProfileTests.cs
+++ src/Services/HardwareServiceOrder/Tests/HardwareServiceOrder.IntegrationTests/HardwareServiceOrderProfileTests.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using HardwareServiceOrder.API.Mappings;
 using HardwareServiceOrder.API.ViewModels;
+using HardwareServiceOrderServices.Models;
 using HardwareServiceOrderServices.ServiceModels;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace HardwareServiceOrder.IntegrationTests
@@ -22,7 +24,7 @@ namespace HardwareServiceOrder.IntegrationTests
         [Fact]
         public void CustomerSettingsToCustomerSettingsDTO()
         {
-            CustomerSettings customerSettings1 = new CustomerSettings
+            API.ViewModels.CustomerSettings customerSettings1 = new API.ViewModels.CustomerSettings
             {
                 CustomerId = System.Guid.NewGuid(),
                 LoanDevice = new LoanDevice
@@ -40,7 +42,7 @@ namespace HardwareServiceOrder.IntegrationTests
         [Fact]
         public void CustomerSettingsToCustomerSettingsDTO_NullLoanDevice()
         {
-            CustomerSettings customerSettings1 = new CustomerSettings
+            API.ViewModels.CustomerSettings customerSettings1 = new API.ViewModels.CustomerSettings
             {
                 CustomerId = System.Guid.NewGuid()
             };
@@ -63,6 +65,76 @@ namespace HardwareServiceOrder.IntegrationTests
             var dto = _mapper.Map<AssetInfoDTO>(asset);
 
             Assert.True(dto.PurchaseDate.HasValue);
+        }
+
+        [Fact]
+        public void MapHardwareViewModelToDto()
+        {
+            var serviceOrder = new API.ViewModels.NewHardwareServiceOrder
+            {
+                ErrorDescription = "sd",
+                OrderedBy = new API.ViewModels.ContactDetails
+                {
+                    FirstName = "sd",
+                    LastName = "sd",
+                    Id = new Guid(),
+                    Email = "sds@as.com",
+                    PartnerId = new Guid(),
+                    PartnerName = "ved",
+                    PartnerOrganizationNumber = "23456",
+                    OrganizationId = new Guid(),
+                    OrganizationName = "AS",
+                    OrganizationNumber = "12",
+                    PhoneNumber = "23"
+                },
+                AssetInfo = new API.ViewModels.AssetInfo
+                {
+                    Imei = "500119468586675",
+                    Accessories = new List<string>
+                    {
+                        "sdsd"
+                    },
+                    AssetCategoryId = 3,
+                    Model = "wwe",
+                    Brand = "wewe",
+                    PurchaseDate = new DateOnly(),
+                    SerialNumber = "wewew",
+                    AssetLifecycleId = new Guid(),
+                    AssetName = "sd"
+                },
+                DeliveryAddress = new API.ViewModels.DeliveryAddress
+                {
+                    Recipient = "fs",
+                    Address1 = "f",
+                    Address2 = "f",
+                    City = "f",
+                    Country = "FS",
+                    PostalCode = "erg",
+                    RecipientType = RecipientTypeEnum.Personal
+                }
+            };
+
+            var serviceOrderDTO = _mapper.Map<HardwareServiceOrderDTO>(serviceOrder);
+
+            Assert.NotNull(serviceOrderDTO);
+            Assert.Equal(serviceOrder.AssetInfo.Imei, serviceOrderDTO.AssetInfo.Imei);
+            Assert.Equal(serviceOrder.AssetInfo.Model, serviceOrderDTO.AssetInfo.Model);
+            Assert.Equal(serviceOrder.AssetInfo.SerialNumber, serviceOrderDTO.AssetInfo.SerialNumber);
+            Assert.Equal(serviceOrder.AssetInfo.Brand, serviceOrderDTO.AssetInfo.Brand);
+            Assert.Equal(serviceOrder.AssetInfo.Accessories, serviceOrderDTO.AssetInfo.Accessories);
+            Assert.Equal(serviceOrder.AssetInfo.AssetCategoryId, serviceOrderDTO.AssetInfo.AssetCategoryId);
+            Assert.Equal(serviceOrder.DeliveryAddress.Address2, serviceOrderDTO.DeliveryAddress.Address2);
+            Assert.Equal(serviceOrder.DeliveryAddress.Address1, serviceOrderDTO.DeliveryAddress.Address1);
+            Assert.Equal(serviceOrder.DeliveryAddress.City, serviceOrderDTO.DeliveryAddress.City);
+            Assert.Equal(serviceOrder.DeliveryAddress.Country, serviceOrderDTO.DeliveryAddress.Country);
+            Assert.Equal(serviceOrder.DeliveryAddress.Recipient, serviceOrderDTO.DeliveryAddress.Recipient);
+            Assert.Equal(serviceOrder.OrderedBy.FirstName, serviceOrderDTO.OrderedBy.FirstName);
+            Assert.Equal(serviceOrder.OrderedBy.LastName, serviceOrderDTO.OrderedBy.LastName);
+            Assert.Equal(serviceOrder.OrderedBy.PartnerName, serviceOrderDTO.OrderedBy.PartnerName);
+            Assert.Equal(serviceOrder.OrderedBy.PartnerOrganizationNumber, serviceOrderDTO.OrderedBy.PartnerOrganizationNumber);
+            Assert.Equal(serviceOrder.OrderedBy.PhoneNumber, serviceOrderDTO.OrderedBy.PhoneNumber);
+            Assert.Equal(serviceOrder.OrderedBy.OrganizationName, serviceOrderDTO.OrderedBy.OrganizationName);
+            Assert.Equal(serviceOrder.ErrorDescription, serviceOrderDTO.ErrorDescription);
         }
     }
 }
--- src/Services/HardwareServiceOrder/Tests/HardwareServiceOrder.UnitTests/HardwareServiceOrderServiceTests.cs
+++ src/Services/HardwareServiceOrder/Tests/HardwareServiceOrder.UnitTests/HardwareServiceOrderServiceTests.cs
@@ -1,8 +1,6 @@
 ﻿using AutoMapper;
 using Common.Configuration;
 using Common.Utilities;
-using HardwareServiceOrder.API.Mappings;
-using HardwareServiceOrder.API.ViewModels;
 using HardwareServiceOrderServices;
 using HardwareServiceOrderServices.Configuration;
 using HardwareServiceOrderServices.Email;
@@ -16,7 +14,6 @@ using Microsoft.Extensions.Options;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Resources;
@@ -28,13 +25,8 @@ namespace HardwareServiceOrder.UnitTests
     public class HardwareServiceOrderServiceTests : HardwareServiceOrderServiceBaseTests
     {
         private readonly IMapper? _mapper;
-
-        private readonly IMapper mapper;
         private readonly IHardwareServiceOrderService _hardwareServiceOrderService;
         private readonly HardwareServiceOrderContext _dbContext;
-        private readonly IEmailService _emailService;
-        private readonly OrigoConfiguration _origoConfiguration;
-        private readonly ProviderFactory _providerFactory;
         public HardwareServiceOrderServiceTests() : base(new DbContextOptionsBuilder<HardwareServiceOrderContext>()
 
         .UseSqlite("Data Source=sqlitehardwareserviceorderservicetests.db").Options)
@@ -47,72 +39,44 @@ namespace HardwareServiceOrder.UnitTests
                 });
                 _mapper = mappingConfig.CreateMapper();
             }
-            if (mapper == null)
-            {
-                var mappingConfig = new MapperConfiguration(mc =>
-                {
-                    mc.AddMaps(Assembly.GetAssembly(typeof(API.Mappings.HardwareServiceOrderProfile)));
-                });
-                mapper = mappingConfig.CreateMapper();
-            }
 
             _dbContext = new HardwareServiceOrderContext(ContextOptions);
+
             var hardwareServiceRepository = new HardwareServiceOrderRepository(_dbContext);
-            var statusHandlers = new Dictionary<ServiceStatusEnum, ServiceOrderStatusHandlerService>
-            {
 
-            };
-
-            var repairPro = new Mock<IRepairProvider>();
-
-            var resourceManger = new ResourceManager("HardwareServiceOrderServices.Resources.HardwareServiceOrder", Assembly.GetAssembly(typeof(EmailService)));
-            var emailOptions = Options.Create(new EmailConfiguration
-            {
-                BaseUrl = "https://origov2dev.mytos.no"
-            });
-            _origoConfiguration = new OrigoConfiguration
-            {
-                BaseUrl = "https://origov2dev.mytos.no",
-                OrderPath = "/my-business/{0}/hardware-repair/{1}/view"
-            };
-            var origoOptions = Options.Create(_origoConfiguration);
-            var flatDictionary = new FlatDictionary();
-
-
-            var _mailmapper = new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(new EmailProfile());
-            }).CreateMapper();
-
+            //Email service mock
             var emailService = new Mock<IEmailService>();
-
             emailService.Setup(x => x.SendOrderConfirmationEmailAsync(new HardwareServiceOrderServices.Email.Models.OrderConfirmationEmail(), "en"));
 
-            _emailService = new EmailService(emailOptions, flatDictionary, resourceManger, _mailmapper, origoOptions, _dbContext);
-
-
-
+            //Repair provider mock
             var repairProviderMock = new Mock<IRepairProvider>();
+
             repairProviderMock.Setup(m => m.CreateRepairOrderAsync(It.IsAny<NewExternalRepairOrderDTO>(), It.IsAny<int>(), It.IsAny<string>()))
                 .ReturnsAsync(new NewExternalRepairOrderResponseDTO(serviceProviderOrderId1: "serviceProviderOrderId1", serviceProviderOrderId2: "serviceProviderOrderId2", externalServiceManagementLink: "externalServiceManagementLink"));
+
             repairProviderMock.Setup(m => m.GetUpdatedRepairOrdersAsync(It.IsAny<DateTimeOffset>()))
-                .ReturnsAsync(new List<ExternalRepairOrderDTO> { new ExternalRepairOrderDTO
-                { AssetIsReplaced = false, ExternalServiceEvents = new List<ExternalServiceEventDTO>{ new ExternalServiceEventDTO {  ServiceStatusId = 2, Timestamp = DateTimeOffset.UtcNow} },
-                    ProvidedAsset = new AssetInfoDTO(), ReturnedAsset = new AssetInfoDTO(), ServiceProviderOrderId1 = "serviceProviderOrderId1", ServiceProviderOrderId2 = "serviceProviderOrderId2"} });
+                .ReturnsAsync(new List<ExternalRepairOrderDTO> {
+                    new ExternalRepairOrderDTO
+                    {
+                        AssetIsReplaced = false,
+                        ExternalServiceEvents = new List<ExternalServiceEventDTO>{ new ExternalServiceEventDTO {  ServiceStatusId = 2, Timestamp = DateTimeOffset.UtcNow} },
+                        ProvidedAsset = new AssetInfoDTO(),
+                        ReturnedAsset = new AssetInfoDTO(),
+                        ServiceProviderOrderId1 = "serviceProviderOrderId1",
+                        ServiceProviderOrderId2 = "serviceProviderOrderId2"
+                    }
+                });
 
             var providerFactoryMock = new Mock<IProviderFactory>();
 
             providerFactoryMock.Setup(m => m.GetRepairProviderAsync(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>()))
                 .ReturnsAsync(repairProviderMock.Object);
 
+            //Status handler mock
             var statusHandlMock = new Dictionary<ServiceStatusEnum, ServiceOrderStatusHandlerService>();
-
             var serviceOrderStatusHandlerServiceMock = new Mock<ServiceOrderStatusHandlerService>();
-            serviceOrderStatusHandlerServiceMock.Setup(m => m.UpdateServiceOrderStatusAsync(It.IsAny<Guid>(), It.IsAny<ServiceStatusEnum>(), It.IsAny<ISet<string>>(), It.IsAny<string>()));
-
+            serviceOrderStatusHandlerServiceMock.Setup(m => m.UpdateServiceOrderStatusAsync(It.IsAny<Guid>(), It.IsAny<ServiceStatusEnum>(), It.IsAny<IEnumerable<string>>(), It.IsAny<string>()));
             statusHandlMock.Add(ServiceStatusEnum.Unknown, serviceOrderStatusHandlerServiceMock.Object);
-
-            //statusHandlMock.Setup(m => m. (ServiceStatusEnum.Unknown, It.IsAny<ServiceOrderStatusHandlerService>()));
 
             _hardwareServiceOrderService = new HardwareServiceOrderService(hardwareServiceRepository, _mapper, providerFactoryMock.Object, statusHandlMock, emailService.Object);
 
@@ -141,85 +105,6 @@ namespace HardwareServiceOrder.UnitTests
             var order = await _dbContext.HardwareServiceOrders.FirstOrDefaultAsync();
             Assert.NotNull(order);
             Assert.NotNull(order!.Owner);
-        }
-
-        [Fact]
-        public async Task MapHardwareViewModelToDto()
-        {
-            var serviceOrder = new API.ViewModels.NewHardwareServiceOrder
-            {
-                ErrorDescription = "sd",
-                OrderedBy = new API.ViewModels.ContactDetailsExtended
-                {
-                    FirstName = "sd",
-                    LastName = "sd",
-                    UserId = new Guid(),
-                    Email = "sds@as.com",
-                    PartnerId = new Guid(),
-                    PartnerName = "ved",
-                    PartnerOrganizationNumber = "23456",
-                    OrganizationId = new Guid(),
-                    OrganizationName = "AS",
-                    OrganizationNumber = "12",
-                    PhoneNumber = "23"
-                },
-                AssetInfo = new API.ViewModels.AssetInfo
-                {
-                    Imei = "500119468586675",
-                    //AssetLifecycleId = new Guid(),
-                    Accessories = new List<string>
-                    {
-                        "sdsd"
-                    },
-                    AssetCategoryId = 3,
-                    Model = "wwe",
-                    Brand = "wewe",
-                    PurchaseDate = new DateOnly(),
-                    SerialNumber = "wewew",
-                    AssetLifecycleId = new Guid(),
-                    AssetName = "sd"
-                },
-                DeliveryAddress = new API.ViewModels.DeliveryAddress
-                {
-                    Recipient = "fs",
-                    Address1 = "f",
-                    Address2 = "f",
-                    City = "f",
-                    Country = "FS",
-                    PostalCode = "erg",
-                    RecipientType = RecipientTypeEnum.Personal
-                }
-            };
-
-            var serviceOrderDTO = mapper.Map<NewHardwareServiceOrderDTO>(serviceOrder);
-
-
-            Assert.NotNull(serviceOrderDTO);
-
-            Assert.Equal(serviceOrder.AssetInfo.Imei, serviceOrderDTO.AssetInfo.Imei);
-            Assert.Equal(serviceOrder.AssetInfo.Model, serviceOrderDTO.AssetInfo.Model);
-            Assert.Equal(serviceOrder.AssetInfo.SerialNumber, serviceOrderDTO.AssetInfo.SerialNumber);
-            Assert.Equal(serviceOrder.AssetInfo.Brand, serviceOrderDTO.AssetInfo.Brand);
-            Assert.Equal(serviceOrder.AssetInfo.Accessories, serviceOrderDTO.AssetInfo.Accessories);
-            Assert.Equal(serviceOrder.AssetInfo.AssetCategoryId, serviceOrderDTO.AssetInfo.AssetCategoryId);
-
-            //Assert.Equal(serviceOrder.AssetInfo.PurchaseDate, serviceOrderDTO.AssetInfo.PurchaseDate);
-
-            Assert.Equal(serviceOrder.DeliveryAddress.Address2, serviceOrderDTO.DeliveryAddress.Address2);
-            Assert.Equal(serviceOrder.DeliveryAddress.Address1, serviceOrderDTO.DeliveryAddress.Address1);
-            Assert.Equal(serviceOrder.DeliveryAddress.City, serviceOrderDTO.DeliveryAddress.City);
-            Assert.Equal(serviceOrder.DeliveryAddress.Country, serviceOrderDTO.DeliveryAddress.Country);
-            Assert.Equal(serviceOrder.DeliveryAddress.Recipient, serviceOrderDTO.DeliveryAddress.Recipient);
-
-
-            Assert.Equal(serviceOrder.OrderedBy.FirstName, serviceOrderDTO.OrderedBy.FirstName);
-            Assert.Equal(serviceOrder.OrderedBy.LastName, serviceOrderDTO.OrderedBy.LastName);
-            Assert.Equal(serviceOrder.OrderedBy.PartnerName, serviceOrderDTO.OrderedBy.PartnerName);
-            Assert.Equal(serviceOrder.OrderedBy.PartnerOrganizationNumber, serviceOrderDTO.OrderedBy.PartnerOrganizationNumber);
-            Assert.Equal(serviceOrder.OrderedBy.PhoneNumber, serviceOrderDTO.OrderedBy.PhoneNumber);
-            Assert.Equal(serviceOrder.OrderedBy.OrganizationName, serviceOrderDTO.OrderedBy.OrganizationName);
-
-            Assert.Equal(serviceOrder.ErrorDescription, serviceOrderDTO.ErrorDescription);
         }
 
         [Fact]
@@ -289,7 +174,7 @@ namespace HardwareServiceOrder.UnitTests
         {
             await _hardwareServiceOrderService.UpdateOrderStatusAsync();
 
-            var orders = _hardwareServiceOrderService.GetHardwareServiceOrdersAsync(CUSTOMER_ONE_ID,null,false, new System.Threading.CancellationToken()).Result.Items;
+            var orders = _hardwareServiceOrderService.GetHardwareServiceOrdersAsync(CUSTOMER_ONE_ID, null, false, new System.Threading.CancellationToken()).Result.Items;
 
             Assert.NotNull(orders);
 
