dc633dabd3474844de1755447e3834608bb0daaf
Christian <christian@mytos.no>
2022-06-22 15:24:58 +0200

    Added IMEI validation attribute

--- src/Services/HardwareServiceOrder/HardwareServiceOrder.API/Extensions/SwaggerGenOptionsExtensions.cs
+++ src/Services/HardwareServiceOrder/HardwareServiceOrder.API/Extensions/SwaggerGenOptionsExtensions.cs
@@ -3,6 +3,13 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace HardwareServiceOrder.API.Extensions
 {
+    /// <summary>
+    ///     Adds support for the <see cref="DateOnly"/> and <see cref="TimeOnly"/> data-types in the Swagger UI. <para>
+    ///     
+    ///     When the extension is registered with Swagger, these data-types will correctly be presented as '<c>string</c>'
+    ///     values instead of a complex object, with the correct <c>format</c> specification also added to the OpenAPI-schema
+    ///     (either '<c>date</c>' or '<c>time</c>'). </para>
+    /// </summary>
     public static class SwaggerGenOptionsExtensions
     {
         /// <summary>
---
+++ src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Attributes/ImeiAttribute.cs
@@ -0,0 +1,80 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HardwareServiceOrderServices.Attributes
+{
+    /// <summary>
+    ///     Applies IMEI validation to a <see cref="string"/> or <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    /// <remarks>
+    ///     This only checks if existing values are valid. <br/>
+    ///       - To prevent <see langword="null"/> values, use <see cref="RequiredAttribute"/> and/or nullable-notations. <br/>
+    ///       - Use <see cref="MinLengthAttribute"/> to enforce the number of items in a <see cref="IEnumerable{T}"/>.
+    /// </remarks>
+    public class ImeiAttribute : ValidationAttribute
+    {
+        private static readonly string _imeiRegexPattern = "^[0-9]{14,15}$";
+
+
+        /// <inheritdoc/>
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            // No need to validate nulls. This should be done using the [Required] attribute and/or nullable annotations.
+            if (value is null)
+                return ValidationResult.Success;
+            // IEnumerable (JS arrays)
+            else if (value is IEnumerable<string>)
+                return ValidateValue((IEnumerable<string>)value);
+            // Good ol' strings
+            else if (value is string)
+                return ValidateValue((string)value);
+
+            // If it don't fall into one of the above, it's likely wrongly tagged, so we just skip the check and return success.
+            return ValidationResult.Success;
+        }
+
+
+        /// <summary>
+        ///     Validate single-value string inputs.
+        /// </summary>
+        /// <param name="value"> The input to be validated. </param>
+        /// <returns> The validation result. </returns>
+        private static ValidationResult? ValidateValue(string value)
+        {
+            if (IsValidImei(value))
+                return new ValidationResult($"The value '{value}' is not a valid IMEI number.");
+
+            return ValidationResult.Success;
+        }
+
+        /// <summary>
+        ///     Validate the value of all items contained in a IEnumerable.
+        /// </summary>
+        /// <param name="value"> The input to be validated. </param>
+        /// <returns> The validation result. </returns>
+        private static ValidationResult? ValidateValue(IEnumerable<string> value)
+        {
+            // Ensure all items in the enumerator are valid
+            foreach (var listItem in value)
+            {
+                if (!IsValidImei(listItem))
+                    return new ValidationResult($"The value '{listItem}' is not a valid IMEI number.");
+            }
+
+            return ValidationResult.Success;
+        }
+
+
+        /// <summary>
+        ///     Checks if a given string contains a valid IMEI-number.
+        /// </summary>
+        /// <param name="imei"> The content that should be validated. </param>
+        /// <returns> Returns <see langword="true"/> if the string contains a valid IMEI number. 
+        ///     Otherwise it returns <see langword="false"/>. </returns>
+        private static bool IsValidImei(in string imei)
+        {
+            // TODO: Extend this to also check the value/validate the IMEI using the Luhn-algorithm.
+            return Regex.IsMatch(imei, _imeiRegexPattern);
+        }
+
+    }
+}
