23d48a9a7d6b1e4297fe7338bca1d65e1938de06
Atish Kumar Dipongkor <atish@brainstation-23.com>
2022-06-24 22:18:00 +0600

    gateway model updated

--- src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/HardwareServiceOrder/LoanDevice.cs
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/HardwareServiceOrder/LoanDevice.cs
@@ -8,5 +8,9 @@ namespace OrigoApiGateway.Models.HardwareServiceOrder
     {
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        /// <summary>
+        /// This property ensures whether a customer provides loan device
+        /// </summary>
+        public bool ProvidesLoanDevice { get; set; }
     }
 }
