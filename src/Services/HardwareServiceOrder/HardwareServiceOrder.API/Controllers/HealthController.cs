69cce99765f7638dcc48a6a0ff92a6b38ea60438
Christian <christian@mytos.no>
2022-06-27 16:01:25 +0200

    Added basic health endpoints that is also used for additional unit-test checks (header validations)

---
+++ src/Services/HardwareServiceOrder/HardwareServiceOrder.API/Controllers/HealthController.cs
@@ -0,0 +1,112 @@
+﻿using HardwareServiceOrderServices.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HardwareServiceOrder.API.Controllers
+{
+    /// <summary>
+    ///     A health-controller used for ensuring the microservice is reached, and that the requests are valid.
+    /// </summary>
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly IApiRequesterService _apiRequesterService;
+
+
+        public HealthController(IApiRequesterService apiRequesterService)
+        {
+            _apiRequesterService = apiRequesterService;
+        }
+
+
+        private object GetResponseObject()
+        {
+            return new
+            {
+                AuthenticatedUserId = _apiRequesterService.AuthenticatedUserId
+            };
+        }
+
+
+        /// <summary>
+        ///     Perform a test-connection using a GET request.
+        /// </summary>
+        /// <remarks>
+        ///     See if the GET request is accepted or rejected. Rejected requests are likely a result of universally
+        ///     required information missing from the request, such as a missing HTTP-header.
+        /// </remarks>
+        /// <returns> The success or error code for the HTTP request. </returns>
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status200OK)]
+        public async Task<ActionResult> TestGetRequestAsync()
+        {
+            var context = HttpContext;
+            return Ok(GetResponseObject());
+        }
+
+        /// <summary>
+        ///     Perform a test-connection using a POST request.
+        /// </summary>
+        /// <remarks>
+        ///     See if the POST request is accepted or rejected. Rejected requests are likely a result of universally
+        ///     required information missing from the request, such as a missing HTTP-header.
+        /// </remarks>
+        /// <returns> The success or error code for the HTTP request. </returns>
+        [HttpPost]
+        [SwaggerResponse(StatusCodes.Status200OK)]
+        public async Task<ActionResult> TestPostRequestAsync()
+        {
+            var context = HttpContext;
+            return Ok(GetResponseObject());
+        }
+
+        /// <summary>
+        ///     Perform a test-connection using a PUT request.
+        /// </summary>
+        /// <remarks>
+        ///     See if the PUT request is accepted or rejected. Rejected requests are likely a result of universally
+        ///     required information missing from the request, such as a missing HTTP-header.
+        /// </remarks>
+        /// <returns> The success or error code for the HTTP request. </returns>
+        [HttpPut]
+        [SwaggerResponse(StatusCodes.Status200OK)]
+        public async Task<ActionResult> TestPutRequestAsync()
+        {
+            var context = HttpContext;
+            return Ok(GetResponseObject());
+        }
+
+        /// <summary>
+        ///     Perform a test-connection using a PATCH request.
+        /// </summary>
+        /// <remarks>
+        ///     See if the PATCH request is accepted or rejected. Rejected requests are likely a result of universally
+        ///     required information missing from the request, such as a missing HTTP-header.
+        /// </remarks>
+        /// <returns> The success or error code for the HTTP request. </returns>
+        [HttpPatch]
+        [SwaggerResponse(StatusCodes.Status200OK)]
+        public async Task<ActionResult> TestPatchRequestAsync()
+        {
+            var context = HttpContext;
+            return Ok(GetResponseObject());
+        }
+
+        /// <summary>
+        ///     Perform a test-connection using a DELETE request.
+        /// </summary>
+        /// <remarks>
+        ///     See if the DELETE request is accepted or rejected. Rejected requests are likely a result of universally
+        ///     required information missing from the request, such as a missing HTTP-header.
+        /// </remarks>
+        /// <returns> The success or error code for the HTTP request. </returns>
+        [HttpDelete]
+        [SwaggerResponse(StatusCodes.Status200OK)]
+        public async Task<ActionResult> TestDeleteRequestAsync()
+        {
+            var context = HttpContext;
+            return Ok(GetResponseObject());
+        }
+    }
+}
---
+++ src/Services/HardwareServiceOrder/Tests/HardwareServiceOrder.IntegrationTests/Controllers/HealthControllerTests.cs
@@ -0,0 +1,304 @@
+﻿using HardwareServiceOrder.API.Controllers;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace HardwareServiceOrder.IntegrationTests.Controllers
+{
+    public class HealthControllerTests : IClassFixture<HardwareServiceOrderWebApplicationFactory<HealthController>>
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly HttpClient _httpClient;
+        private readonly string url = $"/api/v1/health";
+
+
+        public HealthControllerTests(HardwareServiceOrderWebApplicationFactory<HealthController> factory, ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+            _httpClient = factory.CreateDefaultClient();
+        }
+
+
+        /// <summary>
+        ///     Makes sure that the 'X-Authenticated-UserId' is correctly updated between API calls.
+        ///     This is done by doing several requests, using different values. <para>
+        ///     
+        ///     We want to ensure that the dependency-injection correctly applies and returns the expected
+        ///     value for the current caller, ensuring we don't accidentally set it to an incorrect lifetime
+        ///     in the injections, that would result in a fixed value being applied. </para>
+        /// </summary>
+        /// <returns> The awaitable task. </returns>
+        [Fact(DisplayName = "Test if 'X-Authenticated-UserId' changes")]
+        public async Task TestThatAuthenticatedUserIdChangesAsync()
+        {
+            // Arrange
+            _testOutputHelper.WriteLine($"URL: {url}");
+            HttpResponseMessage firstResponse, secondResponse;
+            string firstJsonResponse, secondJsonResponse;
+
+            // Act
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, url))
+            {
+                requestMessage.Headers.Clear();
+                requestMessage.Headers.Add("X-Authenticated-UserId", "10000000-0000-0000-0000-000000000000");
+                firstResponse = await _httpClient.SendAsync(requestMessage);
+
+                firstJsonResponse = await firstResponse.Content.ReadAsStringAsync();
+            }
+
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, url))
+            {
+                requestMessage.Headers.Clear();
+                requestMessage.Headers.Add("X-Authenticated-UserId", "20000000-0000-0000-0000-000000000000");
+                secondResponse = await _httpClient.SendAsync(requestMessage);
+
+                secondJsonResponse = await secondResponse.Content.ReadAsStringAsync();
+            }
+
+            // Asserts
+            Assert.Contains("10000000-0000-0000-0000-000000000000", firstJsonResponse);
+            Assert.Contains("20000000-0000-0000-0000-000000000000", secondJsonResponse);
+        }
+
+
+        /// <summary>
+        ///     Verifies the default value for the 'X-Authenticated-UserId' header parameter, where the HTTP request 
+        ///     don't supply the value. This is tested on a API endpoint method (GET) that do not require the value
+        ///     to be supplied.
+        /// </summary>
+        /// <returns> The awaitable task. </returns>
+        [Fact(DisplayName = "Test default 'X-Authenticated-UserId' value")]
+        public async Task TestDefaultUserIdAsync()
+        {
+            // Arrange
+            _testOutputHelper.WriteLine($"URL: {url}");
+            HttpResponseMessage response;
+            string jsonResponse;
+
+            // Act
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Get, url))
+            {
+                requestMessage.Headers.Clear();
+                response = await _httpClient.SendAsync(requestMessage);
+
+                jsonResponse = await response.Content.ReadAsStringAsync();
+            }
+
+            // Asserts
+            Assert.Contains(Guid.Empty.ToString(), jsonResponse);
+        }
+
+
+        /// <summary>
+        ///     Checks if the <see cref="HardwareServiceOrderServices.Infrastructure.IApiRequesterService"/> in the controller(s)
+        ///     correctly applies the header requirement checks for GET-requests.
+        /// </summary>
+        /// <returns> The awaitable task. </returns>
+        [Fact(DisplayName = "Test GET request w/o headers")]
+        public async Task TestGetRequestWithoutHeaderAsync()
+        {
+            // Arrange
+            _testOutputHelper.WriteLine($"URL: {url}");
+            HttpResponseMessage response;
+
+            // Act
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Get, url))
+            {
+                requestMessage.Headers.Clear();
+                response = await _httpClient.SendAsync(requestMessage);
+            }
+
+            // Asserts
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+        /// <summary>
+        ///     Checks if the <see cref="HardwareServiceOrderServices.Infrastructure.IApiRequesterService"/> in the controller(s)
+        ///     correctly applies the header requirement checks for GET-requests.
+        /// </summary>
+        /// <returns> The awaitable task. </returns>
+        [Fact(DisplayName = "Test GET request w/headers")]
+        public async Task TestGetRequestWithHeaderAsync()
+        {
+            // Arrange
+            _testOutputHelper.WriteLine($"URL: {url}");
+            HttpResponseMessage responseWithHeaders;
+
+            // Act
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Get, url))
+            {
+                requestMessage.Headers.Clear();
+                requestMessage.Headers.Add("X-Authenticated-UserId", "b94f4697-9439-47b5-884a-906ca6bf909f");
+                responseWithHeaders = await _httpClient.SendAsync(requestMessage);
+            }
+
+            // Asserts
+            Assert.Equal(HttpStatusCode.OK, responseWithHeaders.StatusCode);
+        }
+
+
+        /// <summary>
+        ///     Checks if the <see cref="HardwareServiceOrderServices.Infrastructure.IApiRequesterService"/> in the controller(s)
+        ///     correctly applies the header requirement checks for POST-requests.
+        /// </summary>
+        /// <returns> The awaitable task. </returns>
+        [Fact(DisplayName = "Test POST request w/o headers")]
+        public async Task TestPostRequestWithoutHeaderAsync()
+        {
+            // Arrange
+            _testOutputHelper.WriteLine($"URL: {url}");
+            var jsonContent = JsonContent.Create("");
+            HttpResponseMessage response;
+
+            // Act
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, url))
+            {
+                requestMessage.Content = jsonContent;
+                requestMessage.Headers.Clear();
+                response = await _httpClient.SendAsync(requestMessage);
+            }
+
+            // Asserts
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        /// <summary>
+        ///     Checks if the <see cref="HardwareServiceOrderServices.Infrastructure.IApiRequesterService"/> in the controller(s)
+        ///     correctly applies the header requirement checks for POST-requests.
+        /// </summary>
+        /// <returns> The awaitable task. </returns>
+        [Fact(DisplayName = "Test POST request w/headers")]
+        public async Task TestPostRequestWithHeaderAsync()
+        {
+            // Arrange
+            _testOutputHelper.WriteLine($"URL: {url}");
+            var jsonContent = JsonContent.Create("");
+            HttpResponseMessage response;
+
+            // Act
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, url))
+            {
+                requestMessage.Content = jsonContent;
+                requestMessage.Headers.Clear();
+                requestMessage.Headers.Add("X-Authenticated-UserId", "b94f4697-9439-47b5-884a-906ca6bf909f");
+                response = await _httpClient.SendAsync(requestMessage);
+            }
+
+            // Asserts
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+        /// <summary>
+        ///     Checks if the <see cref="HardwareServiceOrderServices.Infrastructure.IApiRequesterService"/> in the controller(s)
+        ///     correctly applies the header requirement checks for PUT-requests.
+        /// </summary>
+        /// <returns> The awaitable task. </returns>
+        [Fact(DisplayName = "Test PUT request w/o headers")]
+        public async Task TestPutRequestWithoutHeaderAsync()
+        {
+            // Arrange
+            _testOutputHelper.WriteLine($"URL: {url}");
+            var jsonContent = JsonContent.Create("");
+            HttpResponseMessage response;
+
+            // Act
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Put, url))
+            {
+                requestMessage.Content = jsonContent;
+                requestMessage.Headers.Clear();
+                response = await _httpClient.SendAsync(requestMessage);
+            }
+
+            // Asserts
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+
+        /// <summary>
+        ///     Checks if the <see cref="HardwareServiceOrderServices.Infrastructure.IApiRequesterService"/> in the controller(s)
+        ///     correctly applies the header requirement checks for PUT-requests.
+        /// </summary>
+        /// <returns> The awaitable task. </returns>
+        [Fact(DisplayName = "Test PUT request w/headers")]
+        public async Task TestPutRequestWithHeaderAsync()
+        {
+            // Arrange
+            _testOutputHelper.WriteLine($"URL: {url}");
+            var jsonContent = JsonContent.Create("");
+            HttpResponseMessage response;
+
+            // Act
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Put, url))
+            {
+                requestMessage.Content = jsonContent;
+                requestMessage.Headers.Clear();
+                requestMessage.Headers.Add("X-Authenticated-UserId", "b94f4697-9439-47b5-884a-906ca6bf909f");
+                response = await _httpClient.SendAsync(requestMessage);
+            }
+
+            // Asserts
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+        /// <summary>
+        ///     Checks if the <see cref="HardwareServiceOrderServices.Infrastructure.IApiRequesterService"/> in the controller(s)
+        ///     correctly applies the header requirement checks for PATCH-requests.
+        /// </summary>
+        /// <returns> The awaitable task. </returns>
+        [Fact(DisplayName = "Test PATCH request w/o headers")]
+        public async Task TestPatchRequestWithoutHeader()
+        {
+            // Arrange
+            _testOutputHelper.WriteLine($"URL: {url}");
+            var jsonContent = JsonContent.Create("");
+            HttpResponseMessage responseNoHeaders;
+
+            // Act
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Patch, url))
+            {
+                requestMessage.Content = jsonContent;
+                requestMessage.Headers.Clear();
+                responseNoHeaders = await _httpClient.SendAsync(requestMessage);
+            }
+
+            // Asserts
+            Assert.Equal(HttpStatusCode.BadRequest, responseNoHeaders.StatusCode);
+        }
+
+
+        /// <summary>
+        ///     Checks if the <see cref="HardwareServiceOrderServices.Infrastructure.IApiRequesterService"/> in the controller(s)
+        ///     correctly applies the header requirement checks for PATCH-requests.
+        /// </summary>
+        /// <returns> The awaitable task. </returns>
+        [Fact(DisplayName = "Test PATCH request w/headers")]
+        public async Task TestPatchRequestWithHeader()
+        {
+            // Arrange
+            _testOutputHelper.WriteLine($"URL: {url}");
+            var jsonContent = JsonContent.Create("");
+            HttpResponseMessage response;
+
+            // Act
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Patch, url))
+            {
+                requestMessage.Content = jsonContent;
+                requestMessage.Headers.Clear();
+                requestMessage.Headers.Add("X-Authenticated-UserId", "b94f4697-9439-47b5-884a-906ca6bf909f");
+                response = await _httpClient.SendAsync(requestMessage);
+            }
+
+            // Asserts
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+    }
+}
