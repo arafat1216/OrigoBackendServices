4a585f4de70878e19f550fc94ee12987c99caac6
Christian <christian@mytos.no>
2022-06-28 15:46:53 +0200

    Fixes for the EF save interceptor not correctly applying all values

--- src/Core/EntityFramework/SaveContextChangesInterceptor.cs
+++ src/Core/EntityFramework/SaveContextChangesInterceptor.cs
@@ -1,6 +1,7 @@
 ﻿using Common.Seedwork;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 
 namespace Common.EntityFramework
 {
@@ -48,12 +49,11 @@ namespace Common.EntityFramework
 
 
         /// <inheritdoc/>
-        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
         {
             SetAuditProperties(ref eventData);
-            return base.SavingChangesAsync(eventData, result, cancellationToken);
+            return await base.SavingChangesAsync(eventData, result, cancellationToken);
         }
-
 
 
         private void SetAuditProperties(ref DbContextEventData eventData)
@@ -65,25 +65,24 @@ namespace Common.EntityFramework
 
             foreach (var entry in eventData.Context.ChangeTracker.Entries())
             {
+                // We only want to process auditable entities, so let's make sure it is one, and skip the rest
                 var auditableEntity = entry.Entity as Auditable;
-
-                // We only want to process auditable entities
                 if (auditableEntity is null)
                     continue;
 
                 switch (entry.State)
                 {
-                    case Microsoft.EntityFrameworkCore.EntityState.Detached:
+                    case EntityState.Modified:
+                        HandleModified(ref auditableEntity, timestamp);
+                        entry.CurrentValues.SetValues(auditableEntity);
+                        entry.State = EntityState.Modified;
                         break;
-                    case Microsoft.EntityFrameworkCore.EntityState.Unchanged:
+                    case EntityState.Added:
+                        HandleAdded(ref auditableEntity, timestamp);
+                        entry.CurrentValues.SetValues(auditableEntity);
+                        entry.State = EntityState.Added;
                         break;
-                    case Microsoft.EntityFrameworkCore.EntityState.Deleted:
-                        break;
-                    case Microsoft.EntityFrameworkCore.EntityState.Modified:
-                        HandleModified(auditableEntity, timestamp);
-                        break;
-                    case Microsoft.EntityFrameworkCore.EntityState.Added:
-                        HandleAdded(auditableEntity, timestamp);
+                    case EntityState.Deleted:
                         break;
                     default:
                         break;
@@ -91,37 +90,38 @@ namespace Common.EntityFramework
             }
         }
 
+
         /// <summary>
-        ///     Set details for entities with <see cref="Microsoft.EntityFrameworkCore.EntityState.Added"/>.
+        ///     Set details for entities with <see cref="EntityState.Added"/>.
         /// </summary>
-        /// <param name="auditable"> The <see cref="Auditable"/> entity to be processed. </param>
+        /// <param name="auditableEntity"> The <see cref="Auditable"/> entity to be processed. </param>
         /// <param name="timestamp"> The creation timestamp. </param>
-        private void HandleAdded(in Auditable auditable, in DateTimeOffset timestamp)
+        private void HandleAdded<T>(ref T auditableEntity, in DateTimeOffset timestamp) where T : Auditable
         {
             // We should do a safety-check on the existing values in case someone has manually changed the tracking state to 'Added',
             // or manually assigned the values (e.g. in unit tests).
-            if (auditable.DateCreated == default || auditable.DateCreated == null)
-                SetPrivateProperty(auditable, nameof(auditable.DateCreated), timestamp);
-            if (CallerId is not null && (auditable.CreatedBy == default || auditable.CreatedBy == null || auditable.CreatedBy == Guid.Empty))
-                SetPrivateProperty(auditable, nameof(auditable.CreatedBy), CallerId.Value);
+            if (auditableEntity.DateCreated == default || auditableEntity.DateCreated == null)
+                SetPrivateProperty(ref auditableEntity, nameof(auditableEntity.DateCreated), timestamp);
+            if (CallerId is not null && (auditableEntity.CreatedBy == default || auditableEntity.CreatedBy == null || auditableEntity.CreatedBy == Guid.Empty))
+                SetPrivateProperty(ref auditableEntity, nameof(auditableEntity.CreatedBy), CallerId.Value);
 
             // There should not be any updated by info in new entities (as it's never been modified).
-            SetPrivateProperty<Auditable, Guid?>(auditable, nameof(auditable.UpdatedBy), null);
-            SetPrivateProperty<Auditable, DateTimeOffset?>(auditable, nameof(auditable.DateUpdated), null);
+            SetPrivateProperty<T, Guid?>(ref auditableEntity, nameof(auditableEntity.UpdatedBy), null);
+            SetPrivateProperty<T, DateTimeOffset?>(ref auditableEntity, nameof(auditableEntity.DateUpdated), null);
         }
 
 
         /// <summary>
-        ///     Updates the details for entities with <see cref="Microsoft.EntityFrameworkCore.EntityState.Modified"/>.
+        ///     Updates the details for entities with <see cref="EntityState.Modified"/>.
         /// </summary>
-        /// <param name="auditable"> The <see cref="Auditable"/> entity to be processed. </param>
+        /// <param name="auditableEntity"> The <see cref="Auditable"/> entity to be processed. </param>
         /// <param name="timestamp"> The creation timestamp. </param>
-        private void HandleModified<T>(in T auditable, in DateTimeOffset timestamp) where T : Auditable
+        private void HandleModified<T>(ref T auditableEntity, in DateTimeOffset timestamp) where T : Auditable
         {
-            SetPrivateProperty(auditable, nameof(auditable.DateUpdated), timestamp);
+            SetPrivateProperty(ref auditableEntity, nameof(auditableEntity.DateUpdated), timestamp);
 
             if (CallerId is not null)
-                SetPrivateProperty(auditable, nameof(auditable.UpdatedBy), CallerId);
+                SetPrivateProperty(ref auditableEntity, nameof(auditableEntity.UpdatedBy), CallerId);
         }
 
 
@@ -133,7 +133,7 @@ namespace Common.EntityFramework
         /// <param name="entity"> The entity we are using reflections on. </param>
         /// <param name="propertyName"> The '<c>nameof()</c>' for the private property that is accessed. </param>
         /// <param name="newValue"> The new value that will be set. </param>
-        private void SetPrivateProperty<TEntity, TDatatype>(TEntity entity, string propertyName, TDatatype newValue)
+        private void SetPrivateProperty<TEntity, TDatatype>(ref TEntity entity, string propertyName, TDatatype newValue)
         {
             PropertyInfo? propertyInfo = typeof(TEntity).GetProperty(propertyName);
 
