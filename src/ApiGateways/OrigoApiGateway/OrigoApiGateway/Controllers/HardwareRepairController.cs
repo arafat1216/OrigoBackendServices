6f99184d4802853ee3bbb159434db4b6bb6f1eee
Atish Kumar Dipongkor <atish@brainstation-23.com>
2022-07-12 20:33:05 +0600

    permission given to enduser, manager and customer admin

--- src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Controllers/HardwareRepairController.cs
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Controllers/HardwareRepairController.cs
@@ -24,10 +24,12 @@ namespace OrigoApiGateway.Controllers
     public class HardwareRepairController : ControllerBase
     {
         private readonly IHardwareRepairService _hardwareRepairService;
+        private readonly IAssetServices _assetServices;
 
-        public HardwareRepairController(IHardwareRepairService hardwareRepairService)
+        public HardwareRepairController(IHardwareRepairService hardwareRepairService, IAssetServices assetServices)
         {
             _hardwareRepairService = hardwareRepairService;
+            _assetServices = assetServices;
         }
 
         /// <summary>
@@ -125,20 +127,8 @@ namespace OrigoApiGateway.Controllers
         [ProducesResponseType(typeof(HardwareServiceOrder), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> CreateHardwareServiceOrder(Guid customerId, [FromBody] NewHardwareServiceOrder model)
         {
-            var role = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
-            if (role == PredefinedRole.EndUser.ToString() || role == PredefinedRole.DepartmentManager.ToString() || role == PredefinedRole.Manager.ToString())
-            {
+            if (!await HasPermissionToGetOrCreateOrderAsync(customerId, model.AssetId))
                 return Forbid();
-            }
-
-            if (role != PredefinedRole.SystemAdmin.ToString())
-            {
-                var accessList = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "AccessList")?.Value;
-                if (accessList == null || !accessList.Any() || !accessList.Contains(customerId.ToString()))
-                {
-                    return Forbid();
-                }
-            }
 
             var userId = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Actor)?.Value;
 
@@ -148,6 +138,52 @@ namespace OrigoApiGateway.Controllers
             var newOrder = await _hardwareRepairService.CreateHardwareServiceOrderAsync(customerId, userIdGuid, model);
 
             return Ok(newOrder);
+        }
+
+        private async Task<bool> HasPermissionToGetOrCreateOrderAsync(Guid customerId, Guid assetId)
+        {
+            var role = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
+
+            var asset = await _assetServices.GetAssetForCustomerAsync(customerId, assetId, null);
+
+            var accessList = HttpContext.User.Claims.Where(c => c.Type == "AccessList").Select(y => y.Value).ToList() ?? new List<string> { };
+
+            if (asset == null)
+                return false;
+
+            var userId = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Actor)?.Value;
+
+            if (!Guid.TryParse(userId, out Guid userIdGuid))
+                return false;
+
+            //Check whether an enduser has permission
+            if (role == $"{PredefinedRole.EndUser}" && asset.AssetHolderId != userIdGuid)
+            {
+                return false;
+            }
+
+            //Check whether a manager has permission
+            if (new string[] { $"{PredefinedRole.DepartmentManager}", $"{PredefinedRole.Manager}" }.Contains(role))
+            {
+                if (asset.AssetHolderId != userIdGuid)
+                {
+                    if (accessList == null || !accessList.Any() || !accessList.Contains($"{asset.ManagedByDepartmentId}"))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            //For others except SystemAdmin
+            if (role != $"{PredefinedRole.SystemAdmin}")
+            {
+                if (!accessList.Contains($"{customerId}"))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         /// <summary>
@@ -161,25 +197,16 @@ namespace OrigoApiGateway.Controllers
         [ProducesResponseType(typeof(HardwareServiceOrder), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> GetHardwareServiceOrder(Guid customerId, Guid orderId)
         {
-            var role = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
-            if (role == PredefinedRole.EndUser.ToString() || role == PredefinedRole.DepartmentManager.ToString() || role == PredefinedRole.Manager.ToString())
-            {
-                return Forbid();
-            }
-
-            if (role != PredefinedRole.SystemAdmin.ToString())
-            {
-                var accessList = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "AccessList")?.Value;
-                if (accessList == null || !accessList.Any() || !accessList.Contains(customerId.ToString()))
-                {
-                    return Forbid();
-                }
-            }
             var order = await _hardwareRepairService.GetHardwareServiceOrderAsync(customerId, orderId);
+
+            if (order == null)
+                return Forbid();
+
+            if (!await HasPermissionToGetOrCreateOrderAsync(customerId, order.AssetLifecycleId))
+                return Forbid();
 
             return Ok(order);
         }
-
 
         /// <summary>
         /// Gets list of hardware service orders for a customer
