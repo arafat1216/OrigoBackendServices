6a724e8ad9f9d3d8cd189fec8ffb24c77f357fe4
Christian <christian@mytos.no>
2022-07-27 15:27:45 +0200

    Added a configuration file for "EntityV2"

---
+++ src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Infrastructure/EntityConfiguration/EntityBaseConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Common.Seedwork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HardwareServiceOrderServices.Infrastructure.EntityConfiguration
+{
+    /// <summary>
+    ///     Implements the <see cref="IEntityTypeConfiguration{TEntity}"/> for the given <typeparamref name="T"/> entity-type,
+    ///     and configures all shared properties from <see cref="EntityV2"/> and <see cref="Auditable"/>. <para>
+    ///     
+    ///     To configure the inherited entity, overwrite the <see cref="Configure(EntityTypeBuilder{T})"/> method, and add a call
+    ///     to "<c>base.Configure(builder)</c>" inside the override. </para>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class EntityBaseConfiguration<T> : AuditableBaseConfiguration<T> where T : EntityV2
+    {
+
+        public EntityBaseConfiguration(bool isSqlLite) : base(isSqlLite)
+        {
+
+        }
+
+
+        /// <inheritdoc/>
+        public override void Configure(EntityTypeBuilder<T> builder)
+        {
+            // Call the parent that configures the shared properties from the 'Auditable' entity
+            base.Configure(builder);
+
+            // Configure all ID columns so the auto-incremental value starts on 1000.
+            // This frees up a wide range of values that can be used for seeding-data.
+            builder.Property(e => e.Id)
+                   .ValueGeneratedOnAdd()
+                   .UseIdentityColumn(1000);
+        }
+    }
+}
