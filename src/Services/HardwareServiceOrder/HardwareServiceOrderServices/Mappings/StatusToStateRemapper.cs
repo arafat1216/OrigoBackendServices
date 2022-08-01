0f245e88f9907366dfab538d9a2e6950abd78eb9
Christian <christian@mytos.no>
2022-08-01 09:51:58 +0200

    Added a service-status to service-state remapper

---
+++ src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Mappings/StatusToStateRemapper.cs
@@ -0,0 +1,73 @@
+﻿using HardwareServiceOrderServices.Models;
+
+namespace HardwareServiceOrderServices.Mappings
+{
+    /// <summary>
+    ///     Converts a service-status to it's corresponding service-state.
+    /// </summary>
+    public static class StatusToStateRemapper
+    {
+        /// <summary>
+        ///     Retrieves the <see cref="ServiceStateEnum">service-state value</see> that corresponds to the provided value.
+        /// </summary>
+        /// <param name="serviceStatus"> The value that is being converted. </param>
+        /// <returns> The converted value. If the mapping was unsuccessful, it returns <see cref="ServiceStateEnum.Null"/>. </returns>
+        public static ServiceStateEnum GetEnumValue(ServiceStatusEnum serviceStatus)
+        {
+            switch (serviceStatus)
+            {
+                case ServiceStatusEnum.Unknown:
+                    return ServiceStateEnum.Unknown;
+
+                case ServiceStatusEnum.Canceled:
+                    return ServiceStateEnum.Canceled;
+
+                case ServiceStatusEnum.Registered:
+                case ServiceStatusEnum.RegisteredInTransit:
+                case ServiceStatusEnum.RegisteredUserActionNeeded:
+                    return ServiceStateEnum.Registered;
+
+                case ServiceStatusEnum.Ongoing:
+                case ServiceStatusEnum.OngoingUserActionNeeded:
+                case ServiceStatusEnum.OngoingInTransit:
+                case ServiceStatusEnum.OngoingReadyForPickup:
+                    return ServiceStateEnum.Ongoing;
+
+                case ServiceStatusEnum.CompletedNotRepaired:
+                case ServiceStatusEnum.CompletedRepaired:
+                case ServiceStatusEnum.CompletedRepairedOnWarranty:
+                case ServiceStatusEnum.CompletedReplaced:
+                case ServiceStatusEnum.CompletedReplacedOnWarranty:
+                case ServiceStatusEnum.CompletedCredited:
+                case ServiceStatusEnum.CompletedDiscarded:
+                    return ServiceStateEnum.Completed;
+
+                default:
+                    return ServiceStateEnum.Null;
+            }
+        }
+
+        /// <inheritdoc cref="GetEnumValue(ServiceStatusEnum)"/>
+        public static ServiceStateEnum GetEnumValue(int serviceStatus)
+        {
+            ServiceStatusEnum serviceStatusEnum = (ServiceStatusEnum)serviceStatus;
+            return GetEnumValue(serviceStatusEnum);
+        }
+
+
+        /// <returns> The converted value. If the mapping was unsuccessful, it returns 0. </returns>
+        /// <inheritdoc cref="GetEnumValue(ServiceStatusEnum)"/>
+        public static int GetId(ServiceStatusEnum serviceStatus)
+        {
+            return (int)GetEnumValue(serviceStatus);
+        }
+
+
+        /// <inheritdoc cref="GetId(ServiceStatusEnum)"/>
+        public static int GetId(int serviceStatus)
+        {
+            ServiceStatusEnum serviceStatusEnum = (ServiceStatusEnum)serviceStatus;
+            return (int)GetEnumValue(serviceStatusEnum);
+        }
+    }
+}
---
+++ src/Services/HardwareServiceOrder/Tests/HardwareServiceOrder.UnitTests/Mappings/StatusToStateRemapperTests.cs
@@ -0,0 +1,50 @@
+﻿using HardwareServiceOrderServices.Models;
+using Xunit;
+
+namespace HardwareServiceOrderServices.Mappings.Tests
+{
+    public class StatusToStateRemapperTests
+    {
+        [Theory()]
+        [InlineData(ServiceStatusEnum.CompletedRepairedOnWarranty, (int)ServiceStateEnum.Completed)]
+        [InlineData(ServiceStatusEnum.OngoingUserActionNeeded, (int)ServiceStateEnum.Ongoing)]
+        [InlineData(ServiceStatusEnum.Unknown, (int)ServiceStateEnum.Unknown)]
+        [InlineData((ServiceStatusEnum)999, (int)ServiceStateEnum.Null)] // This input value don't exist in the enum
+        public void GetIdTest(ServiceStatusEnum input, int expectedResult)
+        {
+            Assert.Equal(expectedResult, StatusToStateRemapper.GetId(input));
+        }
+
+        [Theory()]
+        [InlineData((int)ServiceStatusEnum.CompletedRepairedOnWarranty, (int)ServiceStateEnum.Completed)]
+        [InlineData((int)ServiceStatusEnum.OngoingUserActionNeeded, (int)ServiceStateEnum.Ongoing)]
+        [InlineData((int)ServiceStatusEnum.Unknown, (int)ServiceStateEnum.Unknown)]
+        [InlineData(999, (int)ServiceStateEnum.Null)] // This input value don't exist in the enum
+        public void GetIdFromIdIdTest(int input, int expectedResult)
+        {
+            Assert.Equal(expectedResult, StatusToStateRemapper.GetId(input));
+        }
+
+        [Theory()]
+        [InlineData(ServiceStatusEnum.CompletedRepairedOnWarranty, ServiceStateEnum.Completed)]
+        [InlineData(ServiceStatusEnum.OngoingUserActionNeeded, ServiceStateEnum.Ongoing)]
+        [InlineData(ServiceStatusEnum.Unknown, ServiceStateEnum.Unknown)]
+        [InlineData((ServiceStatusEnum)999, ServiceStateEnum.Null)] // This input value don't exist in the enum
+
+        public void GetEnumValueFromEnumTest(ServiceStatusEnum input, ServiceStateEnum expectedResult)
+        {
+            Assert.Equal(expectedResult, StatusToStateRemapper.GetEnumValue(input));
+        }
+
+        [Theory()]
+        [InlineData((int)ServiceStatusEnum.CompletedRepairedOnWarranty, ServiceStateEnum.Completed)]
+        [InlineData((int)ServiceStatusEnum.OngoingUserActionNeeded, ServiceStateEnum.Ongoing)]
+        [InlineData((int)ServiceStatusEnum.Unknown, ServiceStateEnum.Unknown)]
+        [InlineData(999, ServiceStateEnum.Null)] // This input value don't exist in the enum
+        public void GetEnumValueFromIdTest(int input, ServiceStateEnum expectedResult)
+        {
+            Assert.Equal(expectedResult, StatusToStateRemapper.GetEnumValue(input));
+
+        }
+    }
+}
