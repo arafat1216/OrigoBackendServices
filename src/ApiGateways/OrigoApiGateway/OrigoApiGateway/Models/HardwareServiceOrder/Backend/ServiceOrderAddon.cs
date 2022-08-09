8ffccc9edcb8b789888dffe7bcdb1dc23a394d12
Christian <christian@mytos.no>
2022-08-09 10:00:47 +0200

    Added the API gateway API entities

---
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/HardwareServiceOrder/Backend/ServiceOrderAddon.cs
@@ -0,0 +1,58 @@
+﻿#nullable enable
+
+using OrigoApiGateway;
+
+namespace OrigoApiGateway.Models.HardwareServiceOrder.Backend
+{
+    /// <summary>
+    ///     Contains the details for a single service-addon that is offered by a <c>ServiceProvider</c>.
+    /// </summary>
+    public class ServiceOrderAddon
+    {
+        /// <summary>
+        ///     The ID that uniquely identifies this service-order addon.
+        /// </summary>
+        [Required]
+        [SwaggerSchema(ReadOnly = true)]
+        public int Id { get; init; }
+
+        /// <summary>
+        ///     The ID of the <c>ServiceProvider</c> that offer the service-addon.
+        /// </summary>
+        [Required]
+        public int ServiceProviderId { get; set; }
+
+        /// <summary>
+        ///     Is this an option that the users themselves can choose to include when they are placing service-orders?
+        /// 
+        ///     <para>
+        ///     When <c><see langword="true"/></c>, the user that places the order may choose to include this service-addon when 
+        ///     creating a new service-order. All user selectable service-addons are opt-in, and is not included in the service-order
+        ///     unless the user has explicitly added it to the order. </para>
+        ///     
+        ///     <para>
+        ///     If <see langword="false"/>, then the service-addon is always included. </para>
+        /// </summary>
+        /// <remarks>
+        ///     NB: This setting is only applicable if the service-addon has been enabled on the customer!
+        /// </remarks>
+        /// <value>
+        ///     A <see cref="bool"/> that indicates if the service-addon is enforced, or selectable by the user when placing service-orders.
+        /// </value>
+        public bool IsUserSelectable { get; set; }
+
+        /// <summary>
+        ///     Can customers turn this addon on/off for their own organization using the administration APIs?
+        /// 
+        ///     <para>
+        ///     When <see langword="true"/>, then customers should be able to toggle the service on/off in their organization settings/APIs. </para>
+        ///     
+        ///     <para>
+        ///     When <see langword="false"/>, it means that this is a system/backoffice-only setting. </para>
+        /// </summary>
+        /// <value>
+        ///     A <see cref="bool"/> that indicates if customers are allowed to enable/disable this service-addon for their own organization.
+        /// </value>
+        public bool IsCustomerTogglable { get; set; }
+    }
+}
---
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/HardwareServiceOrder/Backend/ServiceProvider.cs
@@ -0,0 +1,53 @@
+﻿#nullable enable
+
+using OrigoApiGateway.Models.HardwareServiceOrder.Backend;
+
+namespace OrigoApiGateway.Models.HardwareServiceOrder.Backend
+{
+    /// <summary>
+    ///     Contains information about a single service-provider.
+    /// </summary>
+    public class ServiceProvider
+    {
+        /// <summary>
+        ///     The service-provider's identifier.
+        /// </summary>
+        /// <value>
+        ///     The service-provider's identifier.
+        /// </value>
+        [Required]
+        [SwaggerSchema(ReadOnly = true)]
+        public int Id { get; init; }
+
+        /// <summary>
+        ///     The service-provider's name (non-localized).
+        /// </summary>
+        [MaxLength(256)]
+        [Required]
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        ///     The ID for the service-provider's <c><see cref="Organization"/></c> entity.
+        /// </summary>
+        [Required]
+        public Guid OrganizationId { get; set; }
+
+        /// <summary>
+        ///     A list containing all service-addons that is offered by the service-provider.
+        /// </summary>
+        /// <value>
+        ///     If <c><see langword="null"/></c>, then the has not been explicitly requested. 
+        ///     Otherwise, it will return a collection detailing the service-order addons that is offered by this service-provider.
+        /// </value>
+        public ICollection<ServiceOrderAddon>? OfferedServiceOrderAddons { get; set; }
+
+        /// <summary>
+        ///     A set containing the IDs for all service-types that's supported by the service-provider.
+        /// </summary>
+        /// <value>
+        ///     If <c><see langword="null"/></c>, then the has not been explicitly requested. 
+        ///     Otherwise, it will return a set detailing the service-type IDs that can be used with this service-provider.
+        /// </value>
+        public ISet<int>? SupportedServiceTypeIds { get; set; }
+    }
+}
