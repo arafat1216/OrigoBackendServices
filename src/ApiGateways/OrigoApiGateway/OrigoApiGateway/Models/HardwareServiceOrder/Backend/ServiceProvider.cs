21aedd3710ed73a6a3b32ae7225b82afa89c379b
Christian <christian@mytos.no>
2022-08-09 13:27:07 +0200

    Fixed PR comments

--- src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/HardwareServiceOrder/Backend/ServiceProvider.cs
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/HardwareServiceOrder/Backend/ServiceProvider.cs
@@ -1,7 +1,5 @@
 ﻿#nullable enable
 
-using OrigoApiGateway.Models.HardwareServiceOrder.Backend;
-
 namespace OrigoApiGateway.Models.HardwareServiceOrder.Backend
 {
     /// <summary>
@@ -36,8 +34,9 @@ namespace OrigoApiGateway.Models.HardwareServiceOrder.Backend
         ///     A list containing all service-addons that is offered by the service-provider. 
         ///     
         ///     <para>
-        ///     If <c><see langword="null"/></c>, then the has not been explicitly requested. 
-        ///     Otherwise, it will return a collection detailing the service-order addons that is offered by this service-provider. </para>
+        ///     A collection detailing the service-order addons that is offered by this service-provider.
+        ///     For most requests where this list has not been explicitly requested, the value will be <c><see langword="null"/></c>.
+        /// </para>
         /// </summary>
         public ICollection<ServiceOrderAddon>? OfferedServiceOrderAddons { get; set; }
 
@@ -45,8 +44,8 @@ namespace OrigoApiGateway.Models.HardwareServiceOrder.Backend
         ///     A set containing the IDs for all service-types that's supported by the service-provider.
         /// </summary>
         /// <value>
-        ///     If <c><see langword="null"/></c>, then the has not been explicitly requested. 
-        ///     Otherwise, it will return a set detailing the service-type IDs that can be used with this service-provider.
+        ///     A set detailing the service-type IDs that can be used with this service-provider.
+        ///     For most requests where this list has not been explicitly requested, the value will be <c><see langword="null"/></c>.
         /// </value>
         public ISet<int>? SupportedServiceTypeIds { get; set; }
     }
