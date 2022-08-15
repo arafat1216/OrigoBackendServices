7e86a60003898742af90c053a043061ee6a415dc
Christian <christian@mytos.no>
2022-08-15 16:25:45 +0200

    Updated Swagger's "Global Header Parameter" config to support HTTP DELETE. The value is now also required, and have a better description.

--- src/Services/HardwareServiceOrder/HardwareServiceOrder.API/SwaggerGlobalHeaderParameters.cs
+++ src/Services/HardwareServiceOrder/HardwareServiceOrder.API/SwaggerGlobalHeaderParameters.cs
@@ -15,24 +15,34 @@ namespace HardwareServiceOrder.API
             if (operation.Parameters == null)
                 operation.Parameters = new List<OpenApiParameter>();
 
-            // If the HttpMethod is POST, PUT or PATCH, add the header-filter
+            // If the HttpMethod is POST, PUT, PATCH or DELETE, add the header-filter
             if (string.Equals(context.ApiDescription.HttpMethod, HttpMethod.Post.Method, StringComparison.InvariantCultureIgnoreCase)
                 || string.Equals(context.ApiDescription.HttpMethod, HttpMethod.Put.Method, StringComparison.InvariantCultureIgnoreCase)
                 || string.Equals(context.ApiDescription.HttpMethod, HttpMethod.Patch.Method, StringComparison.InvariantCultureIgnoreCase)
+                || string.Equals(context.ApiDescription.HttpMethod, HttpMethod.Delete.Method, StringComparison.InvariantCultureIgnoreCase)
             )
             {
                 // Add the "X-Authenticated-UserId" to all related endpoints in Swagger.
                 operation.Parameters.Add(new OpenApiParameter()
                 {
                     Name = "X-Authenticated-UserId",
-                    Description = "The <code>userID</code> of the user/caller that triggered the request.<br/><br/>In supported endpoints, the <code>CreatedBy</code>, <code>UpdatedBy</code> and <code>DeletedBy</code> values is automatically handled, and uses this ID when changes are made in the database.",
+                    Description = "The <code>userID</code> of the user/caller that triggered the request.<br/><br/>" +
+
+                                  "In supported endpoints, the <code>CreatedBy</code>, <code>UpdatedBy</code> and <code>DeletedBy</code> values is automatically handled, and uses this ID to record the user that made changes in the database.<br/><br/>" +
+
+                                  "The following caller IDs exist and are reserved for special cases:<br/>" +
+                                  "<ul>" +
+                                    "<li><code>00000000-0000-0000-0000-000000000000</code> - the caller/user-ID is unknown.</li>" +
+                                    "<li><code>00000000-0000-0000-0000-000000000001</code> - the system's caller/user-ID. Used for automated or M2M processes that's not initiated by a user.</li>" +
+                                  "</ul>",
+                    
                     In = ParameterLocation.Header,
-                    //Required = true,
+                    Required = true,
 
                     Schema = new OpenApiSchema()
                     {
                         Type = "string",
-                        Format = "uuid"
+                        Format = "uuid",
                     },
 
                     // Add a example if we are in debug, as this indicates local environment, and it adds a pre-filled value the developer
