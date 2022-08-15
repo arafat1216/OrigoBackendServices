f21ad2d6e10b8fedfef2b07124aeabdb0aa2a20c
Christian <christian@mytos.no>
2022-08-15 16:22:10 +0200

    Added a generic "Not Found" exception for when the provided user details don't result in any matches.

---
+++ src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Exceptions/NotFoundException.cs
@@ -0,0 +1,28 @@
+﻿namespace HardwareServiceOrderServices.Exceptions
+{
+    /// <summary>
+    ///     Thrown when a request was aborted because one or more provided values was not found during a database lookup.
+    /// </summary>
+    public class NotFoundException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotFoundException"/> class.
+        /// </summary>
+        public NotFoundException() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotFoundException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message"> The message that describes the error. </param>
+        public NotFoundException(string message) : base(message) { }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="NotFoundException"/> class with a specified error 
+        ///     message and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message"> The error message that explains the reason for the exception. </param>
+        /// <param name="innerException">  The exception that is the cause of the current exception, or a <see langword="null"/> reference
+        ///     if no inner exception is specified. </param>
+        public NotFoundException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
