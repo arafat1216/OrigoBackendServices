03dd3df0edaa7dfec359b98a0849c47de80db38a
Fahim Khan <fahim@brainstation-23.com>
2022-08-22 19:38:55 +0600

    missing migrations

---
+++ src/Services/Asset/AssetServices/Migrations/20220822132708_purchasedBy_Column_in_AssetLifeCycle.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetServices.Migrations
+{
+    public partial class purchasedBy_Column_in_AssetLifeCycle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisposeSetting_PayrollContactEmail",
+                table: "CustomerSettings");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PurchasedBy",
+                table: "AssetLifeCycles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PurchasedBy",
+                table: "AssetLifeCycles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DisposeSetting_PayrollContactEmail",
+                table: "CustomerSettings",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
