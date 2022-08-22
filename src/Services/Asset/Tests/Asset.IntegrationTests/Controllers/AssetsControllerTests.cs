7d5c9bc09a9775e49f7c7b0031390b7bbee85233
Fahim Khan <fahim@brainstation-23.com>
2022-08-22 15:52:57 +0600

    Unit test for 'PurchasedBy'

--- src/Services/Asset/Tests/Asset.IntegrationTests/Controllers/AssetsControllerTests.cs
+++ src/Services/Asset/Tests/Asset.IntegrationTests/Controllers/AssetsControllerTests.cs
@@ -339,6 +339,59 @@ public class AssetsControllerTests : IClassFixture<AssetWebApplicationFactory<St
     }
 
     [Fact]
+    public async Task CreateAssetWithoutPurchasedBy()
+    {
+        var newAsset = new NewAsset
+        {
+            Alias = "Just another name",
+            AssetCategoryId = 1,
+            Note = "A long note",
+            Brand = "iPhone",
+            ProductName = "12 Pro Max",
+            LifecycleType = LifecycleType.Transactional,
+            PurchaseDate = new DateTime(2022, 2, 2),
+            ManagedByDepartmentId = Guid.NewGuid(),
+            AssetHolderId = Guid.NewGuid(),
+            Imei = new List<long> { 356728115537645 },
+            CallerId = _callerId
+        };
+        _testOutputHelper.WriteLine(JsonSerializer.Serialize(newAsset));
+        var requestUri = $"/api/v1/Assets/customers/{_organizationId}";
+        _testOutputHelper.WriteLine(requestUri);
+        var createResponse = await _httpClient.PostAsJsonAsync(requestUri, newAsset);
+        var message = await createResponse.Content.ReadAsStringAsync();
+        _testOutputHelper.WriteLine(message);
+        Assert.Equal(HttpStatusCode.Created, createResponse.StatusCode);
+    }
+
+    [Fact]
+    public async Task CreateAssetWithPurchasedBy()
+    {
+        var newAsset = new NewAsset
+        {
+            Alias = "Just another name",
+            AssetCategoryId = 1,
+            Note = "A long note",
+            Brand = "iPhone",
+            ProductName = "12 Pro Max",
+            LifecycleType = LifecycleType.Transactional,
+            PurchaseDate = new DateTime(2022, 2, 2),
+            ManagedByDepartmentId = Guid.NewGuid(),
+            AssetHolderId = Guid.NewGuid(),
+            Imei = new List<long> { 356728115537645 },
+            CallerId = _callerId,
+            PurchasedBy = "techstep"
+        };
+        _testOutputHelper.WriteLine(JsonSerializer.Serialize(newAsset));
+        var requestUri = $"/api/v1/Assets/customers/{_organizationId}";
+        _testOutputHelper.WriteLine(requestUri);
+        var createResponse = await _httpClient.PostAsJsonAsync(requestUri, newAsset);
+        var assetReturned = await createResponse.Content.ReadFromJsonAsync<API.ViewModels.Asset>();
+        Assert.Equal(HttpStatusCode.Created, createResponse.StatusCode);
+        Assert.Equal(assetReturned!.PurchasedBy, newAsset.PurchasedBy);
+    }
+
+    [Fact]
     public async Task CreateAssetWithIsPersonalSetAndNoAssetTag()
     {
         var newAsset = new NewAsset
