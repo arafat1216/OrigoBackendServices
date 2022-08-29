62c17bd978f926cc5ec2071f37b8d603330c4cfc
Christian <christian@mytos.no>
2022-08-29 11:05:42 +0200

    Added db migrations

---
+++ src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Migrations/20220829083104_AddedApiCredentials.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HardwareServiceOrderServices.Migrations
+{
+    public partial class AddedApiCredentials : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApiCredentials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1000000, 1"),
+                    CustomerServiceProviderId = table.Column<int>(type: "int", nullable: false),
+                    ServiceTypeId = table.Column<int>(type: "int", nullable: false),
+                    ApiUsername = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ApiPassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastUpdateFetched = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    DateCreated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false, defaultValueSql: "SYSUTCDATETIME()"),
+                    DateUpdated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    DateDeleted = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiCredentials", x => x.Id);
+                    table.UniqueConstraint("AK_ApiCredentials_CustomerServiceProviderId_ServiceTypeId", x => new { x.CustomerServiceProviderId, x.ServiceTypeId });
+                    table.ForeignKey(
+                        name: "FK_ApiCredentials_CustomerServiceProvider_CustomerServiceProviderId",
+                        column: x => x.CustomerServiceProviderId,
+                        principalTable: "CustomerServiceProvider",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ApiCredentials_ServiceTypes_ServiceTypeId",
+                        column: x => x.ServiceTypeId,
+                        principalTable: "ServiceTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApiCredentials_ServiceTypeId",
+                table: "ApiCredentials",
+                column: "ServiceTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApiCredentials");
+        }
+    }
+}
