a923238e539ba318dd4298534129c1da5a01d9ee
Christian <christian@mytos.no>
2022-09-12 15:40:04 +0200

    Added DB migrations

---
+++ src/Services/HardwareServiceOrder/HardwareServiceOrderServices/Migrations/20220912132850_AddedOrderedServiceAddonsToServiceOrders.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HardwareServiceOrderServices.Migrations
+{
+    public partial class AddedOrderedServiceAddonsToServiceOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IncludedServiceOrderAddonIds",
+                table: "HardwareServiceOrders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "ServiceOrderAddons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ThirdPartyId",
+                value: "268");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IncludedServiceOrderAddonIds",
+                table: "HardwareServiceOrders");
+
+            migrationBuilder.UpdateData(
+                table: "ServiceOrderAddons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ThirdPartyId",
+                value: "");
+        }
+    }
+}
