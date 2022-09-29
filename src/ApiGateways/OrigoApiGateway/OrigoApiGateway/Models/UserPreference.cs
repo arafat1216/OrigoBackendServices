3cff4ade67dd5a3292185be76da30da43f489952
miakri <43073917+miakri@users.noreply.github.com>
2022-09-29 08:46:28 +0200

    Added the same regEx to frontend model as for the backend model.

--- src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/UserPreference.cs
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/UserPreference.cs
@@ -9,6 +9,8 @@
         {
             Language = string.Empty;
         }
+        
+        [RegularExpression(@"^[a-z]{2}", ErrorMessage = "Language code needs to be exactly 2 lowercase characters.")] // Exactly 2 lowercase characters
         public string Language { get; set; }
     }
 }
