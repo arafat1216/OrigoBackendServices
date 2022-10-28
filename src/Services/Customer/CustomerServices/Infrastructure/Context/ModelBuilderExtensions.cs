7275c9adf3d4931885261a4b1352ff8b8373457d
Rolf Sjødal <rolf@sers.digital>
2022-10-28 13:26:12 +0200

    Seed permission data

--- src/Services/Customer/CustomerServices/Infrastructure/Context/ModelBuilderExtensions.cs
+++ src/Services/Customer/CustomerServices/Infrastructure/Context/ModelBuilderExtensions.cs
@@ -1,35 +1,100 @@
-﻿using CustomerServices.Models;
+﻿using System.Reflection;
+using Common.Enums;
+using CustomerServices.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
 
-namespace CustomerServices.Infrastructure.Context
+namespace CustomerServices.Infrastructure.Context;
+
+public static class ModelBuilderExtensions
 {
-    public static class ModelBuilderExtensions
+    public static void Seed(this ModelBuilder modelBuilder)
     {
-        public static void Seed(this ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Permission>().HasData(
-                new Permission(1, "CanCreateCustomer", new DateTime(2022, 1, 1)),
-                new Permission(2, "CanReadCustomer", new DateTime(2022, 1, 1)),
-                new Permission(3, "CanUpdateCustomer", new DateTime(2022, 1, 1)),
-                new Permission(4, "CanDeleteCustomer", new DateTime(2022, 1, 1))
-            );
-            
-            modelBuilder.Entity<PermissionSet>().HasData(
-                new PermissionSet(1, "FullCustomerAccess", new DateTime(2022, 1, 1))
-            );
+        // Permissions
+        const int CAN_CREATE_CUSTOMER_ID = 1;
+        const int CAN_READ_CUSTOMER_ID = 2;
+        const int CAN_UPDATE_CUSTOMER_ID = 3;
+        const int CAN_DELETE_CUSTOMER_ID = 4;
+        const int CAN_READ_ASSET_ID = 5;
+        const int CAN_CREATE_ASSET_ID = 6;
+        const int CAN_UPDATE_ASSET_ID = 7;
+        const int CAN_READ_ONBOARDING_STATUS_ID = 8;
+        modelBuilder.Entity<Permission>().HasData(
+            new Permission(CAN_CREATE_CUSTOMER_ID, "CanCreateCustomer", new DateTime(2022, 1, 1)),
+            new Permission(CAN_READ_CUSTOMER_ID, "CanReadCustomer", new DateTime(2022, 1, 1)),
+            new Permission(CAN_UPDATE_CUSTOMER_ID, "CanUpdateCustomer", new DateTime(2022, 1, 1)),
+            new Permission(CAN_DELETE_CUSTOMER_ID, "CanDeleteCustomer", new DateTime(2022, 1, 1)),
+            new Permission(CAN_READ_ASSET_ID, "CanReadAsset", new DateTime(2022, 1, 1)),
+            new Permission(CAN_CREATE_ASSET_ID, "CanCreateAsset", new DateTime(2022, 1, 1)),
+            new Permission(CAN_UPDATE_ASSET_ID, "CanUpdateAsset", new DateTime(2022, 1, 1)),
+            new Permission(CAN_READ_ONBOARDING_STATUS_ID, "CanReadOnboardingStatus", new DateTime(2022, 1, 1))
+        );
 
-            modelBuilder.Entity<Role>().HasData(
-                new Role(1, "EndUser", new DateTime(2022, 1, 1)),
-                new Role(2, "DepartmentManager", new DateTime(2022, 1, 1)),
-                new Role(3, "CustomerAdmin", new DateTime(2022, 1, 1)),
-                new Role(4, "GroupAdmin", new DateTime(2022, 1, 1)),
-                new Role(5, "PartnerAdmin", new DateTime(2022, 1, 1)),
-                new Role(7, "PartnerReadOnlyAdmin", new DateTime(2022, 1, 1)),
-                new Role(6, "SystemAdmin", new DateTime(2022, 1, 1)),
-                new Role(8, "Manager", new DateTime(2022, 1, 1)),
-                new Role(9, "Admin", new DateTime(2022, 1, 1))
+        // Roles
+        modelBuilder.Entity<Role>().HasData(
+            new Role((int) PredefinedRole.EndUser, "EndUser", new DateTime(2022, 1, 1)),
+            new Role((int)PredefinedRole.DepartmentManager, "DepartmentManager", new DateTime(2022, 1, 1)),
+            new Role((int)PredefinedRole.CustomerAdmin, "CustomerAdmin", new DateTime(2022, 1, 1)),
+            new Role((int)PredefinedRole.GroupAdmin, "GroupAdmin", new DateTime(2022, 1, 1)),
+            new Role((int)PredefinedRole.PartnerAdmin, "PartnerAdmin", new DateTime(2022, 1, 1)),
+            new Role((int)PredefinedRole.PartnerReadOnlyAdmin, "PartnerReadOnlyAdmin", new DateTime(2022, 1, 1)),
+            new Role((int)PredefinedRole.SystemAdmin, "SystemAdmin", new DateTime(2022, 1, 1)),
+            new Role((int)PredefinedRole.Manager, "Manager", new DateTime(2022, 1, 1)),
+            new Role((int)PredefinedRole.Admin, "Admin", new DateTime(2022, 1, 1))
+        );
+
+        // PermissionSets
+        const int FULL_CUSTOMER_PERMISSION_SET = 1;
+        const int INTERNAL_CUSTOMER_ACCESS_PERMISSION_SET = 2;
+        const int MANAGER_ACCESS_PERMISSION_SET = 3;
+        const int END_USER_ACCESS_PERMISSION_SET = 4;
+
+        modelBuilder.Entity<PermissionSet>().HasData(
+            new List<PermissionSet>
+            {
+                new(FULL_CUSTOMER_PERMISSION_SET, "FullCustomerAccess", new DateTime(2022, 1, 1)),
+                new(INTERNAL_CUSTOMER_ACCESS_PERMISSION_SET, "InternalCustomerAccess", new DateTime(2022, 1, 1)),
+                new(MANAGER_ACCESS_PERMISSION_SET, "ManagerAccess", new DateTime(2022, 1, 1)),
+                new(END_USER_ACCESS_PERMISSION_SET, "EndUserAccess", new DateTime(2022, 1, 1))
+            });
+
+        modelBuilder.Entity<PermissionSet>()
+            .HasMany(p => p.Permissions)
+            .WithMany(p => p.PermissionSets)
+            .UsingEntity(ps => ps.HasData(
+                new { PermissionSetsId = FULL_CUSTOMER_PERMISSION_SET, PermissionsId = CAN_CREATE_CUSTOMER_ID },
+                new { PermissionSetsId = FULL_CUSTOMER_PERMISSION_SET, PermissionsId = CAN_READ_CUSTOMER_ID },
+                new { PermissionSetsId = FULL_CUSTOMER_PERMISSION_SET, PermissionsId = CAN_UPDATE_CUSTOMER_ID },
+                new { PermissionSetsId = FULL_CUSTOMER_PERMISSION_SET, PermissionsId = CAN_DELETE_CUSTOMER_ID },
+                new { PermissionSetsId = FULL_CUSTOMER_PERMISSION_SET, PermissionsId = CAN_READ_ASSET_ID },
+                new { PermissionSetsId = FULL_CUSTOMER_PERMISSION_SET, PermissionsId = CAN_CREATE_ASSET_ID },
+                new { PermissionSetsId = FULL_CUSTOMER_PERMISSION_SET, PermissionsId = CAN_UPDATE_ASSET_ID },
+                new { PermissionSetsId = FULL_CUSTOMER_PERMISSION_SET, PermissionsId = CAN_READ_ONBOARDING_STATUS_ID },
+
+                new { PermissionSetsId = INTERNAL_CUSTOMER_ACCESS_PERMISSION_SET, PermissionsId = CAN_READ_CUSTOMER_ID },
+                new { PermissionSetsId = INTERNAL_CUSTOMER_ACCESS_PERMISSION_SET, PermissionsId = CAN_UPDATE_CUSTOMER_ID },
+                new { PermissionSetsId = INTERNAL_CUSTOMER_ACCESS_PERMISSION_SET, PermissionsId = CAN_READ_ASSET_ID },
+                new { PermissionSetsId = INTERNAL_CUSTOMER_ACCESS_PERMISSION_SET, PermissionsId = CAN_CREATE_ASSET_ID },
+                new { PermissionSetsId = INTERNAL_CUSTOMER_ACCESS_PERMISSION_SET, PermissionsId = CAN_UPDATE_ASSET_ID },
+                new { PermissionSetsId = INTERNAL_CUSTOMER_ACCESS_PERMISSION_SET, PermissionsId = CAN_READ_ONBOARDING_STATUS_ID },
+
+                new { PermissionSetsId = MANAGER_ACCESS_PERMISSION_SET, PermissionsId = CAN_READ_CUSTOMER_ID },
+                new { PermissionSetsId = MANAGER_ACCESS_PERMISSION_SET, PermissionsId = CAN_READ_ASSET_ID },
+                new { PermissionSetsId = MANAGER_ACCESS_PERMISSION_SET, PermissionsId = CAN_UPDATE_ASSET_ID },
+
+                new { PermissionSetsId = END_USER_ACCESS_PERMISSION_SET, PermissionsId = CAN_READ_CUSTOMER_ID },
+                new { PermissionSetsId = END_USER_ACCESS_PERMISSION_SET, PermissionsId = CAN_READ_ASSET_ID },
+                new { PermissionSetsId = END_USER_ACCESS_PERMISSION_SET, PermissionsId = CAN_UPDATE_ASSET_ID }
+            ));
+
+        modelBuilder.Entity<PermissionSet>().HasMany(p => p.Roles).WithMany(r => r.GrantedPermissions).UsingEntity(pr =>
+            pr.HasData(
+                new { GrantedPermissionsId = FULL_CUSTOMER_PERMISSION_SET, RolesId = (int) PredefinedRole.SystemAdmin },
+                new { GrantedPermissionsId = FULL_CUSTOMER_PERMISSION_SET, RolesId = (int) PredefinedRole.PartnerAdmin },
+                new { GrantedPermissionsId = INTERNAL_CUSTOMER_ACCESS_PERMISSION_SET, RolesId = (int) PredefinedRole.CustomerAdmin },
+                new { GrantedPermissionsId = INTERNAL_CUSTOMER_ACCESS_PERMISSION_SET, RolesId = (int) PredefinedRole.Admin },
+                new { GrantedPermissionsId = MANAGER_ACCESS_PERMISSION_SET, RolesId = (int) PredefinedRole.DepartmentManager },
+                new { GrantedPermissionsId = MANAGER_ACCESS_PERMISSION_SET, RolesId = (int) PredefinedRole.Manager },
+                new { GrantedPermissionsId = END_USER_ACCESS_PERMISSION_SET, RolesId = (int) PredefinedRole.EndUser })
             );
-        }
     }
 }
---
+++ src/Services/Customer/CustomerServices/Migrations/20221028111627_permissionSetsSeeding.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerServices.Migrations
+{
+    public partial class permissionSetsSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM PermissionPermissionSet", true);
+            migrationBuilder.Sql("DELETE FROM PermissionSets", true);
+            migrationBuilder.Sql("DELETE FROM PermissionSetRole", true);
+            migrationBuilder.Sql("DELETE FROM [Permissions]", true);
+
+            migrationBuilder.InsertData(
+                table: "PermissionSets",
+                columns: new[] { "Id", "CreatedBy", "CreatedDate", "DeletedBy", "IsDeleted", "LastUpdatedDate", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "FullCustomerAccess", new Guid("00000000-0000-0000-0000-000000000000") },
+                    { 2, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "InternalCustomerAccess", new Guid("00000000-0000-0000-0000-000000000000") },
+                    { 3, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "ManagerAccess", new Guid("00000000-0000-0000-0000-000000000000") },
+                    { 4, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "EndUserAccess", new Guid("00000000-0000-0000-0000-000000000000") }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Permissions",
+                columns: new[] { "Id", "CreatedBy", "CreatedDate", "DeletedBy", "IsDeleted", "LastUpdatedDate", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "CanCreateCustomer", new Guid("00000000-0000-0000-0000-000000000000") },
+                    { 2, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "CanReadCustomer", new Guid("00000000-0000-0000-0000-000000000000") },
+                    { 3, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "CanUpdateCustomer", new Guid("00000000-0000-0000-0000-000000000000") },
+                    { 4, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "CanDeleteCustomer", new Guid("00000000-0000-0000-0000-000000000000") },
+                    { 5, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "CanReadAsset", new Guid("00000000-0000-0000-0000-000000000000") },
+                    { 6, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "CanCreateAsset", new Guid("00000000-0000-0000-0000-000000000000") },
+                    { 7, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "CanUpdateAsset", new Guid("00000000-0000-0000-0000-000000000000") },
+                    { 8, new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "CanReadOnboardingStatus", new Guid("00000000-0000-0000-0000-000000000000") }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PermissionPermissionSet",
+                columns: new[] { "PermissionSetsId", "PermissionsId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 2 },
+                    { 1, 3 },
+                    { 1, 4 },
+                    { 1, 5 },
+                    { 1, 6 },
+                    { 1, 7 },
+                    { 1, 8 },
+                    { 2, 2 },
+                    { 2, 3 },
+                    { 2, 5 },
+                    { 2, 6 },
+                    { 2, 7 },
+                    { 2, 8 },
+                    { 3, 2 },
+                    { 3, 5 },
+                    { 3, 7 },
+                    { 4, 2 },
+                    { 4, 5 },
+                    { 4, 7 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PermissionSetRole",
+                columns: new[] { "GrantedPermissionsId", "RolesId" },
+                values: new object[,]
+                {
+                    { 1, 5 },
+                    { 1, 6 },
+                    { 2, 3 },
+                    { 2, 9 },
+                    { 3, 2 },
+                    { 3, 8 },
+                    { 4, 1 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 1, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 1, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 1, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 1, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 1, 8 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 2, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 2, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 2, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 2, 8 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 3, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 3, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 3, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 4, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 4, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionPermissionSet",
+                keyColumns: new[] { "PermissionSetsId", "PermissionsId" },
+                keyValues: new object[] { 4, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionSetRole",
+                keyColumns: new[] { "GrantedPermissionsId", "RolesId" },
+                keyValues: new object[] { 1, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionSetRole",
+                keyColumns: new[] { "GrantedPermissionsId", "RolesId" },
+                keyValues: new object[] { 1, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionSetRole",
+                keyColumns: new[] { "GrantedPermissionsId", "RolesId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionSetRole",
+                keyColumns: new[] { "GrantedPermissionsId", "RolesId" },
+                keyValues: new object[] { 2, 9 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionSetRole",
+                keyColumns: new[] { "GrantedPermissionsId", "RolesId" },
+                keyValues: new object[] { 3, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionSetRole",
+                keyColumns: new[] { "GrantedPermissionsId", "RolesId" },
+                keyValues: new object[] { 3, 8 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionSetRole",
+                keyColumns: new[] { "GrantedPermissionsId", "RolesId" },
+                keyValues: new object[] { 4, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionSets",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionSets",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionSets",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionSets",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
