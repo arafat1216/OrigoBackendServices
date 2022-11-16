92a91fa2b1dd9a16333b4ad5ae1561c027d06e4b
Odd Magnus Meyer <odd-magnus@mytos.no>
2022-11-16 11:14:53 +0100

    Add JsonpropertyName to scim ListResponse

--- src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/SCIM/ListResponse.cs
+++ src/ApiGateways/OrigoApiGateway/OrigoApiGateway/Models/SCIM/ListResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace OrigoApiGateway.Models.SCIM;
+﻿using System.Text.Json.Serialization;
+
+namespace OrigoApiGateway.Models.SCIM;
 
 /// <summary>
 /// Class that wraps SCIM resources such as e.g. a <see cref="ScimUser"/> in metadata required by the SCIM protocol
@@ -7,35 +9,37 @@ public class ListResponse
 {
     public string Schemas { get; set; } = "urn:ietf:params:scim:api:messages:2.0:ListResponse";
     public int TotalResults { get; set; } = 0;
-    public int StartIndex { get; set; } = 1; // TODO: Necessary, or removable?
-    public int ItemsPerPage { get; set; } = 25; // TODO: Necessary, or removable?
+    public int StartIndex { get; set; } = 1;
+    public int ItemsPerPage { get; set; } = 25;
+
+    [JsonPropertyName("Resources")]
     public List<Resource> Resources { get; set; } = new();
 
-    public ListResponse (Resource resource)
+    public ListResponse(Resource resource)
     {
         TotalResults = 1;
         Resources = new List<Resource>() { resource };
     }
 
-    public ListResponse (List<Resource> resources)
+    public ListResponse(List<Resource> resources)
     {
         Resources = resources;
         TotalResults = Resources.Count;
     }
 
-    public void AddResource (Resource resource)
+    public void AddResource(Resource resource)
     {
         Resources.Add(resource);
         TotalResults++;
     }
 
-    public void RemoveResource (Resource resource)
-    { 
+    public void RemoveResource(Resource resource)
+    {
         Resources.Remove(resource);
         TotalResults--;
     }
 
-    public void AddResources (List<Resource> resources)
+    public void AddResources(List<Resource> resources)
     {
         Resources.AddRange(resources);
         TotalResults = Resources.Count;
